3e13bae726f021bff31944bdbf3b8577c0d4dc28
daveray@gmail.com <daveray@gmail.com>
2008-10-09 20:03:26 +0000

    (1) Minor change (2) Accidently modified the performance timer behavior.
    --HG--
    extra : convert_revision : svn%3A849a83f0-50c2-4045-bdd1-8b1f698b4b18/trunk%4071

--- jsoar/src/org/jsoar/kernel/PerformanceTimer.java
+++ jsoar/src/org/jsoar/kernel/PerformanceTimer.java
@@ -22,7 +22,7 @@ public class PerformanceTimer
      */
     public static void main(String[] args) throws SoarTclException
     {
-        for(int i = 0; i < 1; ++i)
+        for(int i = 0; i < 15; ++i)
         {
             doRun(args);
         }
@@ -36,8 +36,8 @@ public class PerformanceTimer
     {
         Agent agent = new Agent();
         agent.trace.setEnabled(false);
-        //Writer oldWriter = agent.getPrinter().getWriter();
-        //agent.getPrinter().setWriter(new NullWriter(), false);
+        Writer oldWriter = agent.getPrinter().getWriter();
+        agent.getPrinter().setWriter(new NullWriter(), false);
         agent.initialize();
         SoarTclInterface ifc = new SoarTclInterface(agent);
         
@@ -48,7 +48,7 @@ public class PerformanceTimer
         
         agent.decisionCycle.runForever();
         
-        //agent.getPrinter().setWriter(oldWriter, true);
+        agent.getPrinter().setWriter(oldWriter, true);
         ifc.eval("stats");
         ifc.dispose();
     }
