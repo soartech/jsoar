c6fded00e770505f8943d7d384231a1bcc790c1a
Dave Ray <daveray@gmail.com>
2008-12-21 02:47:36 +0000

    Unit test for fix for hang at shutdown when agent running forever.
    --HG--
    extra : convert_revision : svn%3A849a83f0-50c2-4045-bdd1-8b1f698b4b18/trunk%40204

---
+++ jsoar/test/org/jsoar/runtime/ThreadedAgentProxyTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2008  Dave Ray <daveray@gmail.com>
+ *
+ * Created on Dec 20, 2008
+ */
+package org.jsoar.runtime;
+
+
+import org.jsoar.kernel.Agent;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author ray
+ */
+public class ThreadedAgentProxyTest
+{
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test(timeout=5000)
+    public void testShutdownDoesntHangIfAgentIsRunningForever() throws Exception
+    {
+        ThreadedAgentProxy proxy = new ThreadedAgentProxy(new Agent());
+        proxy.getAgent().decider.setWaitsnc(true);
+        proxy.getAgent().getTrace().setWatchLevel(0);
+        proxy.initialize();
+        proxy.runForever(null);
+        Thread.sleep(500);
+        proxy.shutdown();
+    }
+}
