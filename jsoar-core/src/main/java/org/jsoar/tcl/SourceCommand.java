9ce1c45bca8b44173e65bce96967816c7b32ed5a
daveray <none@none>
2009-05-03 01:52:56 +0000

    More fixes for 0.6.0 release
    --HG--
    extra : convert_revision : svn%3A849a83f0-50c2-4045-bdd1-8b1f698b4b18/trunk%40282

--- jsoar-core/src/main/java/org/jsoar/tcl/SourceCommand.java
+++ jsoar-core/src/main/java/org/jsoar/tcl/SourceCommand.java
@@ -5,7 +5,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Stack;
