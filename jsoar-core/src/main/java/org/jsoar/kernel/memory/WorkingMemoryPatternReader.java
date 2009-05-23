5dc7669482d5b1ebe085c8e7306170b0eeb9a810
Bob Marinier <marinier@gmail.com>
2009-05-23 22:27:55 +0000

    print --exact seems to work now. Fixes and style adjustments suggested by Dave. Currently assumes --internal (need to create a map like csoar to support this, I think).
    --HG--
    extra : convert_revision : svn%3A849a83f0-50c2-4045-bdd1-8b1f698b4b18/trunk%40322

--- jsoar-core/src/main/java/org/jsoar/kernel/memory/WorkingMemoryPatternReader.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/memory/WorkingMemoryPatternReader.java
@@ -16,78 +16,77 @@ import com.google.common.base.Predicate;
 public class WorkingMemoryPatternReader
 {
 
-    public static Predicate<Wme> GetPredicate(Agent context, String pattern) throws Exception {
-		Printer printer = context.getPrinter();
-		StringReader reader = new StringReader(pattern);
-		SymbolFactory syms = context.getSymbols();
+    public static Predicate<Wme> GetPredicate(Agent context, String pattern) throws IllegalArgumentException {
+		final Printer printer = context.getPrinter();
+		final StringReader reader = new StringReader(pattern);
+		final SymbolFactory syms = context.getSymbols();
 		
 		Lexer lex = null;
-		// assume null (any value), and override below as necessary
-		Identifier id = null;
-        Object attr = null;
-        Object value = null;
-        
-		try
-        {
+		try {
 			lex = new Lexer(printer, reader);
 			lex.setAllowIds(true);
 			lex.getNextLexeme();
-	        Lexeme idlexeme = lex.getCurrentLexeme();
-	        if(idlexeme.type == LexemeType.IDENTIFIER)
-	        {
-	            id = syms.findIdentifier(idlexeme.id_letter, idlexeme.id_letter);
-	            if(id == null)
-	            {
-	                // TODO: This should be a new exception type
-	                throw new Exception("No such id");
-	            }
-	        }
-	        else if(!idlexeme.toString().equals("*"))
-	        {
-	            // TODO: This should be a new exception type
-	            throw new Exception("First entry must be identifier or '*'");
-	        }
-	        
-	        lex.getNextLexeme();
-	        Lexeme attrlexeme = lex.getCurrentLexeme();
-	        attr = GetPatternValue(syms, attrlexeme);
-	        
-	        lex.getNextLexeme();
-	        Lexeme valuelexeme = lex.getCurrentLexeme();
-	        value = GetPatternValue(syms, valuelexeme);
         }
-		catch(IOException e)
-		{
-		    e.printStackTrace();
+		catch(IOException e) {
+		    e.printStackTrace(); // probably should throw a new exception (e.g., PatternReaderException)
 		}
+			
+        Lexeme idlexeme = lex.getCurrentLexeme();
+        Identifier id = null; // assume null (any value), and override as necessary
+        if(idlexeme.type == LexemeType.IDENTIFIER)
+        {
+            id = syms.findIdentifier(idlexeme.id_letter, idlexeme.id_number);
+            if(id == null)
+            {
+                throw new IllegalArgumentException("No such id " + idlexeme.id_letter + idlexeme.id_number);
+            }
+        }
+        else if(!idlexeme.toString().equals("*"))
+        {
+            throw new IllegalArgumentException("First entry must be identifier or '*'");
+        }
+        try {
+            lex.getNextLexeme();
+        } catch(IOException e) {
+            e.printStackTrace(); // probably should throw a new exception (e.g., PatternReaderException)
+        }
+        Lexeme attrlexeme = lex.getCurrentLexeme();
+        Object attr = getPatternValue(syms, attrlexeme);
+        
+        try {
+            lex.getNextLexeme();
+        } catch(IOException e) {
+            e.printStackTrace(); // probably should throw a new exception (e.g., PatternReaderException)
+        }
+        Lexeme valuelexeme = lex.getCurrentLexeme();
+        Object value = getPatternValue(syms, valuelexeme);
 		
         return Wmes.newMatcher(syms, id, attr, value, -1);
 	}
     
-    private static Object GetPatternValue(SymbolFactory syms, Lexeme l) throws Exception
+    private static Object getPatternValue(SymbolFactory syms, Lexeme l) throws IllegalArgumentException
     {
-        Object value = null;
+        Object value = null; // assume null (any value), and override as necessary
         switch(l.type)
         {
         case IDENTIFIER:
-            value = syms.findIdentifier(l.id_letter, l.id_letter);
+            value = syms.findIdentifier(l.id_letter, l.id_number);
             if(value == null)
             {
-                // TODO: This should be a new exception type
-                throw new Exception("No such id");
+                throw new IllegalArgumentException("No such id " + l.id_letter + l.id_number);
             }
             break;
         case SYM_CONSTANT:
-            if(!l.toString().equals("*"))
+            if(!l.toString().equals("*")) // * means any value, which is null
             {
                 value = l.toString();
             }
             break;
         case FLOAT:
-            value = new Double(l.float_val);
+            value = l.float_val;
             break;
         case INTEGER:
-            value = new Integer(l.int_val);
+            value = l.int_val;
             break;
         default:
             break;
