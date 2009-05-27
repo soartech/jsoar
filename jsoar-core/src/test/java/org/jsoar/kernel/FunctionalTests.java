ab379a324e5637c9b3b04443b973c6e9c277a18b
Jonathan Voigt <voigtjr@gmail.com>
2009-05-27 16:07:51 +0000

    fixed typo in 1144 fix, fixed bug 1011 csoar revision 10565: when a goal is removed due to the GDS changing, the slots in its higher goal are now marked as changed so that impasses can be regenerated. Regression test added.
    --HG--
    extra : convert_revision : svn%3A849a83f0-50c2-4045-bdd1-8b1f698b4b18/trunk%40337

--- jsoar-core/src/test/java/org/jsoar/kernel/FunctionalTests.java
+++ jsoar-core/src/test/java/org/jsoar/kernel/FunctionalTests.java
@@ -418,4 +418,11 @@ public class FunctionalTests
     {
         runTest("testGDSBug1144", -1); // should halt not crash
     }
+    @Test(timeout=10000)
+    public void testGDSBug1011() throws Exception
+    {
+        runTest("testGDSBug1011", -1);
+        assertEquals(8, agent.getProperties().get(SoarProperties.D_CYCLE_COUNT).intValue());
+        assertEquals(19, agent.getProperties().get(SoarProperties.E_CYCLE_COUNT).intValue());
+    }
 }
