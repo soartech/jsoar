19e5cb6c3343e1e0c014a335b90e41f57467d8d1
daveray <none@none>
2009-06-20 19:53:44 +0000

    Test to ensure that reflection-based debugger loading doesn't break accidentally.
    --HG--
    extra : convert_revision : svn%3A849a83f0-50c2-4045-bdd1-8b1f698b4b18/trunk%40403

---
+++ jsoar-debugger/src/test/java/org/jsoar/debugger/DefaultDebuggerProviderTest.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2009 Dave Ray <daveray@gmail.com>
+ *
+ * Created on Jun 20, 2009
+ */
+package org.jsoar.debugger;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author ray
+ */
+public class DefaultDebuggerProviderTest
+{
+
+    @Test
+    public void testThatThisclassIsInTheRightPackageToAvoidBreakingReflection()
+    {
+        assertEquals("The debugger provider's name or package has chanaged, but is referenced reflectively.",
+                org.jsoar.kernel.DefaultDebuggerProvider.DEFAULT_CLASS, DefaultDebuggerProvider.class.getCanonicalName());
+    }
+
+}
