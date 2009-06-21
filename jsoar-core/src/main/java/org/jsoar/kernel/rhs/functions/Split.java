d3ac15c30807ce3fc384b84ddbdc206756d5a394
daveray <none@none>
2009-06-21 17:05:06 +0000

    Added (string) and (split <target> <regex>) RHS functions. Took Bob's advice and made them separate functions.
    --HG--
    extra : convert_revision : svn%3A849a83f0-50c2-4045-bdd1-8b1f698b4b18/trunk%40417

---
+++ jsoar-core/src/main/java/org/jsoar/kernel/rhs/functions/Split.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2009 Dave Ray <daveray@gmail.com>
+ *
+ * Created on Jun 20, 2009
+ */
+package org.jsoar.kernel.rhs.functions;
+
+import java.util.List;
+
+import org.jsoar.kernel.symbols.Symbol;
+
+import com.google.common.collect.Iterators;
+
+/**
+ * String split RHS function implementation.
+ * 
+ * <p>{@code (split <string> <regex>) } 
+ * 
+ * <p>For example, {@code <s> ^result (split |string to split| | |))} will result in:
+ * 
+ * <pre>{@code
+ * ^result
+ *   ^value string
+ *   ^next
+ *     ^value to
+ *     ^next
+ *       ^value split
+ * }</pre>
+ * 
+ * <p>The behavior of the function is equivalent to {@link java.lang.String#split(String, int)}, i.e.
+ * {@code "string to split".split(" ", -1);}
+ * 
+ * @author ray
+ */
+public class Split extends AbstractRhsFunctionHandler
+{
+    public Split()
+    {
+        super("split", 2, 2);
+    }
+
+    /* (non-Javadoc)
+     * @see org.jsoar.kernel.rhs.functions.RhsFunctionHandler#execute(org.jsoar.kernel.rhs.functions.RhsFunctionContext, java.util.List)
+     */
+    @Override
+    public Symbol execute(RhsFunctionContext context, List<Symbol> arguments)
+            throws RhsFunctionException
+    {
+        RhsFunctions.checkArgumentCount(this, arguments);
+        
+        final String target = arguments.get(0).toString();
+        final String regex = arguments.get(1).toString();
+        return RhsFunctions.createLinkedList(context, Iterators.forArray(target.split(regex, -1)));
+    }
+
+}
---
+++ jsoar-core/src/test/java/org/jsoar/kernel/rhs/functions/StringRhsFunctionTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2009 Dave Ray <daveray@gmail.com>
+ *
+ * Created on Jun 20, 2009
+ */
+package org.jsoar.kernel.rhs.functions;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.jsoar.JSoarTest;
+import org.jsoar.kernel.symbols.Identifier;
+import org.jsoar.kernel.symbols.Symbol;
+import org.jsoar.kernel.symbols.Symbols;
+import org.junit.Test;
+
+/**
+ * @author ray
+ */
+public class StringRhsFunctionTest extends JSoarTest
+{
+    @Test public void testReturnsEmptyStringWhenGivenNoArgs() throws Exception
+    {
+        final StringRhsFunction f = new StringRhsFunction();
+        final Symbol result = f.execute(rhsFuncContext, Symbols.asList(syms));
+        assertEquals("", result.asString().getValue());
+    }
+
+    @Test public void testConvertsDoubleToString() throws Exception
+    {
+        final StringRhsFunction f = new StringRhsFunction();
+        final Symbol result = f.execute(rhsFuncContext, Symbols.asList(syms, 3.14159));
+        assertEquals(Double.toString(3.14159), result.asString().getValue());
+    }
+    
+    @Test public void testConvertsIntToString() throws Exception
+    {
+        final StringRhsFunction f = new StringRhsFunction();
+        final Symbol result = f.execute(rhsFuncContext, Symbols.asList(syms, -98));
+        assertEquals(Integer.toString(-98), result.asString().getValue());
+    }
+    
+    @Test public void testConvertsJavaObjectToString() throws Exception
+    {
+        final StringRhsFunction f = new StringRhsFunction();
+        final File o = new File("/path/to/something");
+        final Symbol result = f.execute(rhsFuncContext, Symbols.asList(syms, o));
+        assertEquals(o.toString(), result.asString().getValue());
+    }
+    
+    @Test public void testConvertsIdentifierToString() throws Exception
+    {
+        final StringRhsFunction f = new StringRhsFunction();
+        final Identifier id = syms.createIdentifier('T');
+        final Symbol result = f.execute(rhsFuncContext, Symbols.asList(syms, id));
+        assertEquals(id.toString(), result.asString().getValue());
+    }
+}
