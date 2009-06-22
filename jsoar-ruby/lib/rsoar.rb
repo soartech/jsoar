9498c210592d120f73f1e43154dbf90194eb6b5d
daveray <none@none>
2009-06-22 02:01:06 +0000

    Added Ruby project to build. Just copies files over. Modify rsoar.rb to try to find JSOAR_HOME assuming it's in a JSoar dist.
    --HG--
    extra : convert_revision : svn%3A849a83f0-50c2-4045-bdd1-8b1f698b4b18/trunk%40424

--- jsoar-ruby/lib/rsoar.rb
+++ jsoar-ruby/lib/rsoar.rb
@@ -3,6 +3,12 @@ $LOAD_PATH.unshift File.dirname(__FILE__)
 
 require 'java'
 
+# If we're in a normal JSoar distribution, then JSOAR_HOME should be
+# two directories up
+if !ENV['JSOAR_HOME']
+  ENV['JSOAR_HOME'] = File.join(File.dirname(__FILE__), '..', '..')
+end
+
 if ENV['JSOAR_HOME']
   Dir.glob(File.join(ENV['JSOAR_HOME'], 'lib', '*.jar')).each do |f|
     require f
