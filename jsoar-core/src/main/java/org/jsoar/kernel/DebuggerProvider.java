92012350ce890862a7d44bfb54c384a6d7b4fce8
Dave Ray <daveray@gmail.com>
2009-07-18 03:13:55 +0000

    Added openDebuggerAndWait() method that will open the debugger and wait until it's showing before proceeding. Without this, you get half of the startup trace in the debugger and half in stdout, if you're lucky. Trace setting also get messed with.
    --HG--
    extra : convert_revision : svn%3A849a83f0-50c2-4045-bdd1-8b1f698b4b18/trunk%40513

--- jsoar-core/src/main/java/org/jsoar/kernel/DebuggerProvider.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/DebuggerProvider.java
@@ -6,15 +6,34 @@
 package org.jsoar.kernel;
 
 /**
+ * Interface for an object that knows how to instantiate and attach a debugger to 
+ * a JSoar agent.
+ * 
  * @author ray
  */
 public interface DebuggerProvider
 {
     /**
-     * Opens a debugger and attaches it to the given agent
+     * Opens a debugger and attaches it to the given agent.
      * 
      * @param agent the agent
      * @throws SoarException 
      */
     void openDebugger(Agent agent) throws SoarException;
+    
+    /**
+     * Opens a debugger and attaches it to the given agent. Waits until the debugger 
+     * is fully initialized before proceeding.
+     * 
+     * <p>Note that care must be taken to avoid deadlocks if the debugger is initialized 
+     * on a different thread such as the Swing event dispatch thread. For example, if this
+     * method was called from the the agent thread of {@link ThreadedAgent} there would 
+     * almost certainly be deadlock.
+     * 
+     * @param agent the agent
+     * @throws SoarException if an error occurs during initialization
+     * @throws InterruptedException if the thread is interrupted while waiting for the
+     *      debugger to initialize
+     */
+    void openDebuggerAndWait(Agent agent) throws SoarException, InterruptedException;
 }
