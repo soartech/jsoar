ceb3bbe56ae17b5391de5a6cc3aa900cb8eb0fc8
daveray <none@none>
2009-07-21 01:12:23 +0000

    (1) ProductionFinderTest resources were in wrong location. Worked in Eclipse, but not in ANT. (2) Fixed handling of value attribute in SoarTech XML conversions.
    --HG--
    rename : jsoar-core/src/test/java/org/jsoar/kernel/ProductionFinderTest_testLeftHandSide.soar => jsoar-core/src/test/resources/org/jsoar/kernel/ProductionFinderTest_testLeftHandSide.soar
    rename : jsoar-core/src/test/java/org/jsoar/kernel/ProductionFinderTest_testRightHandSide.soar => jsoar-core/src/test/resources/org/jsoar/kernel/ProductionFinderTest_testRightHandSide.soar
    extra : convert_revision : svn%3A849a83f0-50c2-4045-bdd1-8b1f698b4b18/trunk%40528

--- jsoar-core/src/main/java/org/jsoar/kernel/io/xml/SoarTechWmeToXml.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/io/xml/SoarTechWmeToXml.java
@@ -27,6 +27,7 @@ import org.w3c.dom.Element;
 public class SoarTechWmeToXml
 {
     public static final String TYPE = "type";
+    public static final String VALUE = "value";
     public static final String INTEGER = "integer";
     public static final String DOUBLE = "double";
     public static final String LINK = "link";
