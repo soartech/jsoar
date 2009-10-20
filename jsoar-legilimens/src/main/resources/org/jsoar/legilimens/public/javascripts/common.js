4c0330e961a25e47f8639132d04333a7a2180164
daveray <none@none>
2009-10-20 03:04:59 +0000

    (1) Cleaned up a lot of javascript (2) added simple flash (3) separated agent controls from nav links (4) update trace immediately after executing a command.
    --HG--
    extra : convert_revision : svn%3A849a83f0-50c2-4045-bdd1-8b1f698b4b18/trunk%40550

--- jsoar-legilimens/src/main/resources/org/jsoar/legilimens/public/javascripts/common.js
+++ jsoar-legilimens/src/main/resources/org/jsoar/legilimens/public/javascripts/common.js
@@ -1,10 +1,27 @@
 
-function runAgentCommand(url, command)
-{
-	$.ajax({ 
-        url: url, 
-        type: "POST", 
-        dataType: "text",
-        data: "command=" + command
-    });
-}
+var Legilimens = function() {
+	return {
+		runAgentCommand : function(command)
+		{
+			$.ajax({ 
+		        url: this.commands_url, 
+		        type: "POST", 
+		        dataType: "text",
+		        data: "command=" + command,
+		        success: function(data, textStatus) {
+					$('#flash').html("Executed '" + command + "'").show();
+					if(Legilimens.afterRun != null) {
+						Legilimens.afterRun();
+					}
+				}
+		    });
+		},
+		afterRun : null,
+
+		run : function() { this.runAgentCommand("run"); },
+		step : function() { this.runAgentCommand("step"); },
+		stop : function() { this.runAgentCommand("stop"); },
+		initSoar : function() { this.runAgentCommand("init-soar"); }
+		
+	};
+}();
