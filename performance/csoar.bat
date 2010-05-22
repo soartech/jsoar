8495c3a9cd99e72567e81303a70582355cb333f9
Dave Ray <daveray@gmail.com>
2010-05-21 21:43:58 -0400

    updates to perf run scripts.

--- performance/csoar.bat
+++ performance/csoar.bat
@@ -1,11 +1,14 @@
 echo off
+setlocal
 if not defined CSOAR_HOME (
-   set CSOAR_HOME=C:\Program Files\Soar-Suite-9.0.0\SoarSuite
+   rem set CSOAR_HOME=C:\Program Files\Soar-Suite-9.0.0\SoarSuite\SoarLibrary
+   set CSOAR_HOME=C:\Program Files\Soar\Soar-Suite-9.3.0-win-x86
 )
 copy %1 csoar.temp.soar
 echo watch 0 >> csoar.temp.soar
+echo stats >> csoar.temp.soar
 echo run >> csoar.temp.soar
 echo stats >> csoar.temp.soar
 echo exit >> csoar.temp.soar
 
-"%CSOAR_HOME%\SoarLibrary\bin\TestCLI.exe" csoar.temp.soar
+"%CSOAR_HOME%\bin\TestCLI.exe" csoar.temp.soar
--- performance/jsoar.bat
+++ performance/jsoar.bat
@@ -1,6 +1,10 @@
-echo off
+@echo off
+setlocal
 if not defined JSOAR_HOME (
    set JSOAR_HOME=..\jsoar-build\dist
 )
-call "%JSOAR_HOME%\perftimer.bat" %*
+set JSOAR_MAIN=org.jsoar.kernel.PerformanceTimer
+set JSOAR_OPTS=-server -Xmx512M
+
+call "%JSOAR_HOME%\bin\jsoar.bat" %*
 
