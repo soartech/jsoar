56226c9fc0820d01f8dfeefa41eb96dceba6d30f
Bob Marinier <marinier@gmail.com>
2010-06-11 13:05:45 -0400

    removed unnecessary import

--- jsoar-core/src/test/java/org/jsoar/kernel/FunctionalTests.java
+++ jsoar-core/src/test/java/org/jsoar/kernel/FunctionalTests.java
@@ -8,7 +8,6 @@ package org.jsoar.kernel;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import org.jsoar.JSoarTest;
