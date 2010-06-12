46bc939de3b5dc26845544fbe48f191264814b82
Bob Marinier <marinier@gmail.com>
2010-06-12 14:33:46 -0400

    split out preference tests
    --HG--
    rename : jsoar-core/src/test/resources/org/jsoar/kernel/FunctionalTests_testConflictImpasse.soar => jsoar-core/src/test/resources/org/jsoar/kernel/PreferenceTests_testConflictImpasse.soar
    rename : jsoar-core/src/test/resources/org/jsoar/kernel/FunctionalTests_testConstraintFailureImpasse.soar => jsoar-core/src/test/resources/org/jsoar/kernel/PreferenceTests_testConstraintFailureImpasse.soar
    rename : jsoar-core/src/test/resources/org/jsoar/kernel/FunctionalTests_testORejectsFirst.soar => jsoar-core/src/test/resources/org/jsoar/kernel/PreferenceTests_testORejectsFirst.soar
    rename : jsoar-core/src/test/resources/org/jsoar/kernel/FunctionalTests_testOperatorNoChangeImpasse.soar => jsoar-core/src/test/resources/org/jsoar/kernel/PreferenceTests_testOperatorNoChangeImpasse.soar
    rename : jsoar-core/src/test/resources/org/jsoar/kernel/FunctionalTests_testPreferenceSemantics.soar => jsoar-core/src/test/resources/org/jsoar/kernel/PreferenceTests_testPreferenceSemantics.soar
    rename : jsoar-core/src/test/resources/org/jsoar/kernel/FunctionalTests_testStateNoChangeImpasse.soar => jsoar-core/src/test/resources/org/jsoar/kernel/PreferenceTests_testStateNoChangeImpasse.soar
    rename : jsoar-core/src/test/resources/org/jsoar/kernel/FunctionalTests_testTieImpasse.soar => jsoar-core/src/test/resources/org/jsoar/kernel/PreferenceTests_testTieImpasse.soar

--- jsoar-core/src/test/java/org/jsoar/kernel/FunctionalTests.java
+++ jsoar-core/src/test/java/org/jsoar/kernel/FunctionalTests.java
@@ -269,53 +269,10 @@ public class FunctionalTests extends FunctionalTestHarness
    		agent.getProductions().loadProduction("test (state <s> ^superstate <d>) -{(<s> ^bar <b>) (<s> -^bar { <> <d>})} -->");
    		agent.getProductions().loadProduction("test (state <s> ^superstate nil) -{(<s> ^bar <d>) (<s> -^bar { <> <d>})} -->");
     }
-    
-    @Test(timeout=10000)
-    public void testPreferenceSemantics() throws Exception
-    {
-        runTest("testPreferenceSemantics", -1);
-    }
-    
-    @Test
-    public void testTieImpasse() throws Exception
-    {
-        runTest("testTieImpasse", 1);
-    }
-    
-    @Test
-    public void testConflictImpasse() throws Exception
-    {
-        runTest("testConflictImpasse", 1);
-    }
-    
-    @Test
-    public void testConstraintFailureImpasse() throws Exception
-    {
-        runTest("testConstraintFailureImpasse", 1);
-    }
-    
-    @Test
-    public void testOperatorNoChangeImpasse() throws Exception
-    {
-        runTest("testOperatorNoChangeImpasse", 2);
-    }
-    
-    @Test
-    public void testStateNoChangeImpasse() throws Exception
-    {
-        runTest("testStateNoChangeImpasse", 1);
-    }
-    
+        
     @Test
     public void testInitialState() throws Exception
     {
         runTest("testInitialState", 1);
     }
-        
-    @Test
-    public void testORejectsFirst() throws Exception
-    {
-        runTest("testORejectsFirst", 1);
-    }
-
 }
