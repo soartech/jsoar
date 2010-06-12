6dab56d9ae34386335f4d4218f758b41bc4b2f89
Bob Marinier <marinier@gmail.com>
2010-06-12 14:55:11 -0400

    split out rete tests. I think FunctionalTests now only contains normal runs of complex tests, like TOH, water-jug, and blocks world

--- jsoar-core/src/test/java/org/jsoar/kernel/FunctionalTests.java
+++ jsoar-core/src/test/java/org/jsoar/kernel/FunctionalTests.java
@@ -7,48 +7,15 @@ package org.jsoar.kernel;
 
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.jsoar.util.adaptables.Adaptables;
 import org.junit.Test;
 
 /**
  * @author ray
  */
 public class FunctionalTests extends FunctionalTestHarness
-{    
-    @Test
-    public void testTowersOfHanoiProductionThatCrashesRete() throws Exception
-    {
-        // 9/24/2008 - This production caused a crash in the initial match of the
-        // production. Nothing to test other than that no exceptions are thrown.
-        agent.getProductions().loadProduction("towers-of-hanoi*propose*initialize\n" +
-        "   (state <s> ^superstate nil\n" +
-        "             -^name)\n" +
-        "-->\n" +
-        "   (<s> ^operator <o> +)\n" +
-        "   (<o> ^name initialize-toh)");
-    }
-    
-    @Test
-    public void testTowersOfHanoiProductionThatCausesMaxElaborations() throws Exception
-    {
-        // 9/24/2008 - This production caused a crash in the initial match of the
-        // production. Nothing to test other than that no exceptions are thrown.
-        agent.getProductions().loadProduction("towers-of-hanoi*propose*initialize\n" +
-        "   (state <s> ^superstate nil\n" +
-        "             -^name)\n" +
-        "-->\n" +
-        "   (<s> ^operator <o> +)\n" +
-        "   (<o> ^name initialize-toh)");
-        
-        agent.getProperties().set(SoarProperties.MAX_ELABORATIONS, 5);
-        agent.runFor(1, RunType.DECISIONS);
-        final DecisionCycle dc = Adaptables.adapt(agent, DecisionCycle.class);
-        assertFalse(dc.isHitMaxElaborations());
-    }    
-    
+{
     @Test(timeout=5000)
     public void testWaterJug() throws Exception
     {
