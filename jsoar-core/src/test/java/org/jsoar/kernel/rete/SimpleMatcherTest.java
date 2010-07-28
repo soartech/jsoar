4f865a495cf8250bf9fa595050a562834297b604
Bob Marinier <marinier@gmail.com>
2010-07-28 15:06:30 -0400

    removed unnecessary imports

--- jsoar-core/src/test/java/org/jsoar/kernel/rete/SimpleMatcherTest.java
+++ jsoar-core/src/test/java/org/jsoar/kernel/rete/SimpleMatcherTest.java
@@ -6,7 +6,6 @@
 package org.jsoar.kernel.rete;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
