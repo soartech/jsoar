3efcd46b1724a855a51395bc0b99800229427fe1
Jonathan Voigt <voigtjr@gmail.com>
2010-09-14 20:55:26 -0400

    Converted to options framework.

--- jsoar-core/src/main/java/org/jsoar/kernel/commands/ExciseCommand.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/commands/ExciseCommand.java
@@ -4,6 +4,7 @@
 package org.jsoar.kernel.commands;
 
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.jsoar.kernel.Agent;
@@ -13,8 +14,11 @@ import org.jsoar.kernel.ProductionType;
 import org.jsoar.kernel.SoarException;
 import org.jsoar.kernel.learning.rl.ReinforcementLearning;
 import org.jsoar.util.adaptables.Adaptables;
+import org.jsoar.util.commands.OptionProcessor;
 import org.jsoar.util.commands.SoarCommand;
 import org.jsoar.util.commands.SoarCommandContext;
+
+import com.google.common.collect.Lists;
 
 /**
  * Implementation of the "excise" command.
@@ -23,69 +27,92 @@ import org.jsoar.util.commands.SoarCommandContext;
  */
 public final class ExciseCommand implements SoarCommand
 {
+    private final OptionProcessor<Options> options = OptionProcessor.create();
+    
+    private enum Options
+    {
+        all, chunks, Default, rl, task, Templates, user,
+    }
+    
     private final Agent agent;
 
     public ExciseCommand(Agent agent)
     {
         this.agent = agent;
+        
+        options
+        .newOption(Options.all)
+        .newOption(Options.chunks)
+        .newOption(Options.Default).shortOption('d')
+        .newOption(Options.rl)
+        .newOption(Options.task)
+        .newOption(Options.Templates)
+        .newOption(Options.user)
+        .done();
     }
 
     @Override
     public String execute(SoarCommandContext commandContext, String[] args) throws SoarException
     {
+        List<String> nonOpts = options.process(Lists.newArrayList(args));
+
         final Set<Production> toExcise = new LinkedHashSet<Production>();
         final ProductionManager pm = agent.getProductions();
         boolean doInit = false;
         boolean rl = false;
-        for(int i = 1; i < args.length; ++i)
-        {
-            final String arg = args[i];
-            if("-a".equals(arg) || "--all".equals(arg))
-            {
-                toExcise.addAll(pm.getProductions(null));
-                doInit = true;
-            }
-            else if("-c".equals(arg) || "--chunks".equals(arg))
-            {
-                toExcise.addAll(pm.getProductions(ProductionType.CHUNK));
-            }
-            else if("-d".equals(arg) || "--default".equals(arg))
-            {
-                toExcise.addAll(pm.getProductions(ProductionType.DEFAULT));
-            }
-            else if("-r".equals(arg) || "--rl".equals(arg))
-            {
-                // cli_excise.cpp:DoExcise
-                toExcise.addAll(pm.getProductions(ProductionType.DEFAULT));
-                toExcise.addAll(pm.getProductions(ProductionType.USER));
-                toExcise.addAll(pm.getProductions(ProductionType.CHUNK));
-                rl = true;
-            }
-            else if("-t".equals(arg) || "--task".equals(arg))
-            {
-                toExcise.addAll(pm.getProductions(ProductionType.CHUNK));
-                toExcise.addAll(pm.getProductions(ProductionType.JUSTIFICATION));
-                toExcise.addAll(pm.getProductions(ProductionType.USER));
-            }
-            else if("-T".equals(arg) || "--templates".equals(arg))
-            {
-                toExcise.addAll(pm.getProductions(ProductionType.TEMPLATE));
-            }
-            else if("-u".equals(arg) || "--user".equals(arg))
-            {
-                toExcise.addAll(pm.getProductions(ProductionType.USER));
-            }
-            else
-            {
-                final Production p = pm.getProduction(arg);
-                if(p == null)
-                {
-                    throw new SoarException("No production named '" + arg + "'");
-                }
-                toExcise.add(p);
-            }
-        }
 
+        if (options.has(Options.all))
+        {
+            toExcise.addAll(pm.getProductions(null));
+            doInit = true;
+        }
+        
+        if (options.has(Options.chunks))
+        {
+            toExcise.addAll(pm.getProductions(ProductionType.CHUNK));
+        }
+        
+        if (options.has(Options.Default))
+        {
+            toExcise.addAll(pm.getProductions(ProductionType.DEFAULT));
+        }
+        
+        if (options.has(Options.rl))
+        {
+            // cli_excise.cpp:DoExcise
+            toExcise.addAll(pm.getProductions(ProductionType.DEFAULT));
+            toExcise.addAll(pm.getProductions(ProductionType.USER));
+            toExcise.addAll(pm.getProductions(ProductionType.CHUNK));
+            rl = true;
+        }
+        
+        if (options.has(Options.task))
+        {
+            toExcise.addAll(pm.getProductions(ProductionType.CHUNK));
+            toExcise.addAll(pm.getProductions(ProductionType.JUSTIFICATION));
+            toExcise.addAll(pm.getProductions(ProductionType.USER));
+        }
+            
+        if (options.has(Options.Templates))
+        {
+            toExcise.addAll(pm.getProductions(ProductionType.TEMPLATE));
+        }
+        
+        if (options.has(Options.user))
+        {
+            toExcise.addAll(pm.getProductions(ProductionType.USER));
+        }
+        
+        for (String arg : nonOpts)
+        {
+            final Production p = pm.getProduction(arg);
+            if(p == null)
+            {
+                throw new SoarException("No production named '" + arg + "'");
+            }
+            toExcise.add(p);
+        }
+            
         for(Production p : toExcise)
         {
             if(!rl ||  (rl && p.rlRuleInfo != null))
