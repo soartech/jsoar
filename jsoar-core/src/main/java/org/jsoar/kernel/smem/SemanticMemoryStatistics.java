76d4b601cd87d907a79d5c3e47b520cc3ec28ea3
Jonathan Voigt <voigtjr@gmail.com>
2012-03-20 08:29:40 -0700

    Added copyright, Javadoc

--- jsoar-core/src/main/java/org/jsoar/kernel/smem/SemanticMemoryStatistics.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/smem/SemanticMemoryStatistics.java
@@ -1,12 +1,31 @@
+/*
+ * Copyright (c) 2012 Dave Ray <daveray@gmail.com>
+ *
+ * Created on Mar 3, 2012
+ */
 package org.jsoar.kernel.smem;
 
+/**
+ * Statistics information for SMem.
+ * 
+ * @author voigtjr
+ */
 public interface SemanticMemoryStatistics 
 {
 
+    /**
+     * @return Number of times the <code>retrieve</code> command has been issued.
+     */
     long getRetrieves();
 
+    /**
+     * @return Number of times the <code>query</code> command has been issued.
+     */
     long getQueries();
 
+    /**
+     * @return Number of times the <code>store</code> command has been issued.
+     */
     long getStores();
 
 }
