2908976cf2c547266cd21cb9ff6f22b4d418bcfd
bob.marinier <bob.marinier@soartech.com>
2012-04-12 13:58:28 -0400

    fixed bat file to work with PATH env var that includes spaces on win 7
    * not sure if win 7 introduced a problem, or if I didn't have spaces in
    my PATH when I tried this almost a year ago (I usually run directly with
    an Eclipse launch, not via this bat file, so I haven't noticed any
    problems in my day-to-day usage before)

--- jsoar-build/resources/bin/soarunit.bat
+++ jsoar-build/resources/bin/soarunit.bat
@@ -1,11 +1,11 @@
 @echo off
-rem This is a startup script for SoarUnit. 
+rem This is a startup script for SoarUnit.
 rem
 rem Usage:
 rem
-rem > soarunit --help 
+rem > soarunit --help
 
-setlocal
+setlocal enableextensions enabledelayedexpansion
 
 set HERE=%~dsp0
 set JSOAR_LIB=%HERE%..\lib
@@ -16,13 +16,13 @@ rem SOAR_HOME has to be set to use CSoar
 if defined SOAR_HOME (
   rem Setup all the native lib junk
   set JSOAR_OPTS="-Dsoar.home=%SOAR_HOME%" "-Djava.library.path=%SOAR_HOME%\bin"
-  
+
   rem Stick the right sml.jar at the front of the classpath
   set JSOAR_CLASSPATH=%SOAR_HOME%\share\java\sml.jar;%JSOAR_CLASSPATH%
 
   rem For CSoar debugger to work, bin has to be on the system path
   rem See http://code.google.com/p/soar/issues/detail?id=81
-  set PATH=%SOAR_HOME%\bin;%PATH%
+  set PATH=%SOAR_HOME%\bin;!PATH!
 )
 
 call %HERE%jsoar.bat %*
