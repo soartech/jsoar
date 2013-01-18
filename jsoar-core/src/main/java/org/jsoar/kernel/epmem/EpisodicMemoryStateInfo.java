c478ec1aecd172a6d4c14fb5d1789aa034afad95
chris.kawatsu <chris.kawatsu@soartech.com>
2013-01-18 09:51:59 -0500

    Moved comments for timer wmes.

--- jsoar-core/src/main/java/org/jsoar/kernel/epmem/EpisodicMemoryStateInfo.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/epmem/EpisodicMemoryStateInfo.java
@@ -12,5 +12,19 @@ public class EpisodicMemoryStateInfo
 //    	soar_module::add_module_wme( thisAgent, id->id.epmem_header, thisAgent->epmem_sym_cmd, id->id.epmem_cmd_header );	
 //    	id->id.epmem_result_header = make_new_identifier( thisAgent, 'R', level );
 //    	soar_module::add_module_wme( thisAgent, id->id.epmem_header, thisAgent->epmem_sym_result, id->id.epmem_result_header );
+
+// CK: not implementing timers
+//    	{
+//    	  int64_t my_time = static_cast<int64_t>( thisAgent->epmem_stats->time->get_value() );
+//    	  if ( my_time == 0 )
+//    	  {
+//    		  // special case: pre-initialization
+//    		  my_time = 1;
+//    	  }
+//    	  
+//    	  Symbol* my_time_sym = make_int_constant( thisAgent, my_time );
+//    	  id->id.epmem_time_wme = soar_module::add_module_wme( thisAgent, id->id.epmem_header, thisAgent->epmem_sym_present_id, my_time_sym );
+//    	  symbol_remove_ref( thisAgent, my_time_sym );
+//    	}
 	}
 }
