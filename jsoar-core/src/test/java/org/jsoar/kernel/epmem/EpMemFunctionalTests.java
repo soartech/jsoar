d9063a3ceca9179a4cee0890a3acb38476509e01
bob.marinier <bob.marinier@soartech.com>
2013-01-18 16:21:40 -0500

    added epmem functional tests from UM

---
+++ jsoar-core/src/test/java/org/jsoar/kernel/epmem/EpMemFunctionalTests.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2012 Soar Technology, Inc.
+ *
+ * Created on Jan 18, 2013
+ */
+package org.jsoar.kernel.epmem;
+
+import org.jsoar.kernel.FunctionalTestHarness;
+import org.junit.Test;
+
+/**
+ * @author bob.marinier
+ */
+public class EpMemFunctionalTests extends FunctionalTestHarness
+{
+    @Test
+    public void testCountEpMem() throws Exception
+    {
+        runTest("testCountEpMem", 1693);
+    }
+    
+    @Test
+    public void testHamilton() throws Exception
+    {
+        runTest("testHamilton", 2);
+    }
+    
+    @Test
+    public void testKB() throws Exception
+    {
+        runTest("testKB", 246);
+    }
+}
