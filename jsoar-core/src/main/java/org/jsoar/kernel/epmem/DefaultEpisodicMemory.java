feb7e095699274dd62314e723b50f774a6a09297
Steve Krawczyk <steve.krawczyk@soartech.com>
2013-01-22 07:15:06 -0500

    finished stubbing new episode

--- jsoar-core/src/main/java/org/jsoar/kernel/epmem/DefaultEpisodicMemory.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/epmem/DefaultEpisodicMemory.java
@@ -910,7 +910,15 @@ public class DefaultEpisodicMemory implements EpisodicMemory
 
         if ( new_memory )
         {
-            epmem_new_episode();
+            try
+            {
+                epmem_new_episode();
+            }
+            catch (SQLException e)
+            {
+                logger.error("While recording new epmem episode: " + e.getMessage(), e);
+                agent.getPrinter().error("While recording new epmem episode: " + e.getMessage());
+            }
         }
 
         return new_memory;
@@ -918,8 +926,9 @@ public class DefaultEpisodicMemory implements EpisodicMemory
 
     /**
      * <p>episodic_memory.cpp:2473:epmem_new_episode( agent *my_agent )
+     * @throws SQLException 
      */
-    private void epmem_new_episode()
+    private void epmem_new_episode() throws SQLException
     {
         // if this is the first episode, initialize db components
         epmem_init_db_catch();
@@ -932,8 +941,8 @@ public class DefaultEpisodicMemory implements EpisodicMemory
         //my_agent->epmem_timers->storage->start();
         ////////////////////////////////////////////////////////////////////////////
 
-        //	    epmem_time_id time_counter = my_agent->epmem_stats->time->get_value();
-        //
+        long time_counter = stats.getTime();//my_agent->epmem_stats->time->get_value();
+        
         //	    // provide trace output
         //	    if ( my_agent->sysparams[ TRACE_EPMEM_SYSPARAM ] )
         //	    {
@@ -985,13 +994,202 @@ public class DefaultEpisodicMemory implements EpisodicMemory
                             wmes = epmem_get_augs_of_id( parent_sym, tc );
                             if ( ! wmes.isEmpty() )
                             {
-                                _epmem_store_level(parent_syms, parent_ids, tc, wmes, parent_id, /*time_counter,*/ id_reservations, new_identifiers, epmem_node, epmem_edge );
+                                _epmem_store_level(parent_syms, parent_ids, tc, wmes, parent_id, time_counter, id_reservations, new_identifiers, epmem_node, epmem_edge );
                             }
                             wmes = null;
                         }
                     }
                 }
             }
+            
+            // all inserts
+            {
+                long temp_node;
+
+//    #ifdef EPMEM_EXPERIMENT
+//                epmem_dc_interval_inserts = epmem_node.size() + epmem_edge.size();
+//    #endif
+
+                // nodes
+                while ( !epmem_node.isEmpty() )
+                {
+                   temp_node = epmem_node.element();
+
+                    // add NOW entry
+                    // id = ?, start = ?
+                   db.add_edge_now.setLong(1, temp_node); //my_agent->epmem_stmts_graph->add_node_now->bind_int( 1, (*temp_node) );
+                   db.add_node_now.setLong(2, time_counter); //my_agent->epmem_stmts_graph->add_node_now->bind_int( 2, time_counter );
+                   db.add_node_now.executeUpdate(/*soar_module::op_reinit*/); //my_agent->epmem_stmts_graph->add_node_now->execute( soar_module::op_reinit );
+
+                    // update min
+                   epmem_node_mins.set((int)temp_node-1, time_counter);
+
+                   epmem_node.poll();
+                }
+
+                // edges
+                while ( !epmem_edge.isEmpty() )
+                {
+                    temp_node = epmem_edge.element();
+
+                    // add NOW entry
+                    // id = ?, start = ?
+                    db.add_edge_now.setLong(1, temp_node); //my_agent->epmem_stmts_graph->add_edge_now->bind_int( 1, (*temp_node) );
+                    db.add_edge_now.setLong(2, time_counter); //my_agent->epmem_stmts_graph->add_edge_now->bind_int( 2, time_counter );
+                    db.add_edge_now.executeUpdate(/*soar_module::op_reinit*/);//my_agent->epmem_stmts_graph->add_edge_now->execute( soar_module::op_reinit );
+
+                    // update min
+                    epmem_edge_mins.set((int)temp_node-1,time_counter);
+
+                    db.update_edge_unique_last.setLong(1, Long.MAX_VALUE); //my_agent->epmem_stmts_graph->update_edge_unique_last->bind_int( 1, LLONG_MAX );
+                    db.update_edge_unique_last.setLong(2, temp_node); //my_agent->epmem_stmts_graph->update_edge_unique_last->bind_int( 2, *temp_node );
+                    db.update_edge_unique_last.executeUpdate(/*soar_module::op_reinit*/); //my_agent->epmem_stmts_graph->update_edge_unique_last->execute( soar_module::op_reinit );
+
+                    epmem_edge.poll();
+                }
+            }
+            
+         // all removals
+//            {
+//                epmem_id_removal_map::iterator r;
+//                epmem_time_id range_start;
+//                epmem_time_id range_end;
+//
+//    #ifdef EPMEM_EXPERIMENT
+//                epmem_dc_interval_removes = 0;
+//    #endif
+//
+//                // nodes
+//                r = my_agent->epmem_node_removals->begin();
+//                while ( r != my_agent->epmem_node_removals->end() )
+//                {
+//                    if ( r->second )
+//                    {
+//    #ifdef EPMEM_EXPERIMENT
+//                        epmem_dc_interval_removes++;
+//    #endif
+//
+//                        // remove NOW entry
+//                        // id = ?
+//                        my_agent->epmem_stmts_graph->delete_node_now->bind_int( 1, r->first );
+//                        my_agent->epmem_stmts_graph->delete_node_now->execute( soar_module::op_reinit );
+//
+//                        range_start = (*my_agent->epmem_node_mins)[ r->first - 1 ];
+//                        range_end = ( time_counter - 1 );
+//
+//                        // point (id, start)
+//                        if ( range_start == range_end )
+//                        {
+//                            my_agent->epmem_stmts_graph->add_node_point->bind_int( 1, r->first );
+//                            my_agent->epmem_stmts_graph->add_node_point->bind_int( 2, range_start );
+//                            my_agent->epmem_stmts_graph->add_node_point->execute( soar_module::op_reinit );
+//                        }
+//                        // node
+//                        else
+//                        {
+//                            epmem_rit_insert_interval( my_agent, range_start, range_end, r->first, &( my_agent->epmem_rit_state_graph[ EPMEM_RIT_STATE_NODE ] ) );
+//                        }
+//
+//                        // update max
+//                        (*my_agent->epmem_node_maxes)[ r->first - 1 ] = true;
+//                    }
+//
+//                    r++;
+//                }
+//                my_agent->epmem_node_removals->clear();
+//
+//                // edges
+//                r = my_agent->epmem_edge_removals->begin();
+//                while ( r != my_agent->epmem_edge_removals->end() )
+//                {
+//                    if ( r->second )
+//                    {
+//    #ifdef EPMEM_EXPERIMENT
+//                        epmem_dc_interval_removes++;
+//    #endif
+//
+//                        // remove NOW entry
+//                        // id = ?
+//                        my_agent->epmem_stmts_graph->delete_edge_now->bind_int( 1, r->first );
+//                        my_agent->epmem_stmts_graph->delete_edge_now->execute( soar_module::op_reinit );
+//
+//                        range_start = (*my_agent->epmem_edge_mins)[ r->first - 1 ];
+//                        range_end = ( time_counter - 1 );
+//
+//                        my_agent->epmem_stmts_graph->update_edge_unique_last->bind_int( 1, range_end );
+//                        my_agent->epmem_stmts_graph->update_edge_unique_last->bind_int( 2, r->first );
+//                        my_agent->epmem_stmts_graph->update_edge_unique_last->execute( soar_module::op_reinit );
+//                        // point (id, start)
+//                        if ( range_start == range_end )
+//                        {
+//                            my_agent->epmem_stmts_graph->add_edge_point->bind_int( 1, r->first );
+//                            my_agent->epmem_stmts_graph->add_edge_point->bind_int( 2, range_start );
+//                            my_agent->epmem_stmts_graph->add_edge_point->execute( soar_module::op_reinit );
+//                        }
+//                        // node
+//                        else
+//                        {
+//                            epmem_rit_insert_interval( my_agent, range_start, range_end, r->first, &( my_agent->epmem_rit_state_graph[ EPMEM_RIT_STATE_EDGE ] ) );
+//                        }
+//
+//                        // update max
+//                        (*my_agent->epmem_edge_maxes)[ r->first - 1 ] = true;
+//                    }
+//
+//                    r++;
+//                }
+//                my_agent->epmem_edge_removals->clear();
+//            }
+//
+//            // all in-place lti promotions
+//            {
+//                for ( epmem_symbol_set::iterator p_it=my_agent->epmem_promotions->begin(); p_it!=my_agent->epmem_promotions->end(); p_it++ )
+//                {
+//                    if ( ( (*p_it)->id.smem_time_id == time_counter ) && ( (*p_it)->id.smem_valid == my_agent->epmem_validation ) )
+//                    {
+//                        _epmem_promote_id( my_agent, (*p_it), time_counter );
+//                    }
+//
+//                    symbol_remove_ref( my_agent, (*p_it) );
+//                }
+//                my_agent->epmem_promotions->clear();
+//            }
+//
+//            // add the time id to the times table
+//            my_agent->epmem_stmts_graph->add_time->bind_int( 1, time_counter );
+//            my_agent->epmem_stmts_graph->add_time->execute( soar_module::op_reinit );
+//
+//            my_agent->epmem_stats->time->set_value( time_counter + 1 );
+//
+//            // update time wme on all states
+//            {
+//                Symbol* state = my_agent->bottom_goal;
+//                Symbol* my_time_sym = make_int_constant( my_agent, time_counter + 1 );
+//
+//                while ( state != NULL )
+//                {
+//                    if ( state->id.epmem_time_wme != NIL )
+//                    {
+//                        soar_module::remove_module_wme( my_agent, state->id.epmem_time_wme );
+//                    }
+//
+//                    state->id.epmem_time_wme = soar_module::add_module_wme( my_agent, state->id.epmem_header, my_agent->epmem_sym_present_id, my_time_sym );
+//
+//                    state = state->id.higher_goal;
+//                }
+//
+//                symbol_remove_ref( my_agent, my_time_sym );
+//            }
+//
+//            // clear add/remove maps
+//            {
+//                my_agent->epmem_wme_adds->clear();
+//            }
+//        }
+//
+//        ////////////////////////////////////////////////////////////////////////////
+//        my_agent->epmem_timers->storage->stop();
+//        ////////////////////////////////////////////////////////////////////////////
         }
     }
 
@@ -1005,7 +1203,7 @@ public class DefaultEpisodicMemory implements EpisodicMemory
     // 1. value known in phase one (try reservation)
     // 2. value unknown in phase one, but known at phase two (try assignment adhering to constraint)
     // 3. value unknown in phase one/two (if anything is left, unconstrained assignment)
-    void _epmem_store_level( Queue<SymbolImpl> parent_syms, Queue<Long> parent_ids, Marker tc, List<WmeImpl> wmes, long parent_id, /*epmem_time_id time_counter,*/
+    void _epmem_store_level( Queue<SymbolImpl> parent_syms, Queue<Long> parent_ids, Marker tc, List<WmeImpl> wmes, long parent_id, long time_counter,
             Map<WmeImpl, EpisodicMemoryIdReservation > id_reservations, Set< SymbolImpl > new_identifiers, Queue< Long > epmem_node, Queue< Long > epmem_edge )
     {
         //TODO
