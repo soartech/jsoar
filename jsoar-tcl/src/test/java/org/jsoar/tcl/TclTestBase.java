16ca1a028416cd029c2d8b5336b5ce7d0dc89e10
charles.newton <charles.newton@soartech.com>
2013-01-30 00:18:25 -0500

    More comprehensive TCL tests for CRLF parsing behavior. Conflicts: 	jsoar-tcl/src/test/java/org/jsoar/tcl/TclLineContinuationTest.java
    cherry pick 8f561f20f5a4f1500c4dd6bc8b8ae5178b725a7c plus a few other tweaks from commits near that one

--- jsoar-tcl/src/test/java/org/jsoar/tcl/TclTestBase.java
+++ jsoar-tcl/src/test/java/org/jsoar/tcl/TclTestBase.java
@@ -5,6 +5,8 @@
  */
 package org.jsoar.tcl;
 
+import java.net.URL;
+
 import org.jsoar.kernel.Agent;
 import org.jsoar.kernel.SoarException;
 import org.junit.After;
@@ -23,7 +25,12 @@ public abstract class TclTestBase
 
     protected void sourceTestFile(Class<? extends TclTestBase> childClass, String name) throws SoarException
     {
-        ifc.source(childClass.getResource("/" + childClass.getName().replace('.', '/')  + "_" + name));
+        ifc.source(getSourceTestFile(childClass, name));
+    }
+    
+    protected URL getSourceTestFile(Class<? extends TclTestBase> childClass, String name)
+    {
+        return childClass.getResource("/" + childClass.getName().replace('.', '/')  + "_" + name);
     }
     
     /**
