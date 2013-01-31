b73bf7a9d7d4ed5d1e140cc1550ebb44c44a2f57
chris.kawatsu <chris.kawatsu@soartech.com>
2013-01-31 15:27:41 -0500

    Translated commented symbols to Java.

--- jsoar-core/src/main/java/org/jsoar/kernel/epmem/EpisodicMemorySymbols.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/epmem/EpisodicMemorySymbols.java
@@ -18,71 +18,65 @@ class EpisodicMemorySymbols
     public final SymbolImpl epmem_sym;
     public final SymbolImpl epmem_sym_cmd;
     public final SymbolImpl epmem_sym_result;
-//    
-//    public final Symbol epmem_sym_retrieved;
-//    public final Symbol epmem_sym_status;
-//    public final Symbol epmem_sym_match_score;
-//    public final Symbol epmem_sym_cue_size;
-//    public final Symbol epmem_sym_normalized_match_score;
-//    public final Symbol epmem_sym_match_cardinality;
-//    public final Symbol epmem_sym_memory_id;
+    
+    public final SymbolImpl epmem_sym_retrieved;
+    public final SymbolImpl epmem_sym_status;
+    public final SymbolImpl epmem_sym_match_score;
+    public final SymbolImpl epmem_sym_cue_size;
+    public final SymbolImpl epmem_sym_normalized_match_score;
+    public final SymbolImpl epmem_sym_match_cardinality;
+    public final SymbolImpl epmem_sym_memory_id;
     public final SymbolImpl epmem_sym_present_id;
-//    public final Symbol epmem_sym_no_memory;
-//    public final Symbol epmem_sym_graph_match;
-//    public final Symbol epmem_sym_graph_match_mapping;
-//    public final Symbol epmem_sym_graph_match_mapping_node;
-//    public final Symbol epmem_sym_graph_match_mapping_cue;
-//    public final Symbol epmem_sym_success;
-//    public final Symbol epmem_sym_failure;
-//    public final Symbol epmem_sym_bad_cmd;
-//    
-//    public final Symbol epmem_sym_retrieve;
-//    public final Symbol epmem_sym_next;
-//    public final Symbol epmem_sym_prev;
-//    public final Symbol epmem_sym_query;
-//    public final Symbol epmem_sym_negquery;
-//    public final Symbol epmem_sym_before;
-//    public final Symbol epmem_sym_after;
-//    public final Symbol epmem_sym_prohibit;
+    public final SymbolImpl epmem_sym_no_memory;
+    public final SymbolImpl epmem_sym_graph_match;
+    public final SymbolImpl epmem_sym_graph_match_mapping;
+    public final SymbolImpl epmem_sym_graph_match_mapping_node;
+    public final SymbolImpl epmem_sym_graph_match_mapping_cue;
+    public final SymbolImpl epmem_sym_success;
+    public final SymbolImpl epmem_sym_failure;
+    public final SymbolImpl epmem_sym_bad_cmd;
+    
+    public final SymbolImpl epmem_sym_retrieve;
+    public final SymbolImpl epmem_sym_next;
+    public final SymbolImpl epmem_sym_prev;
+    public final SymbolImpl epmem_sym_query;
+    public final SymbolImpl epmem_sym_negquery;
+    public final SymbolImpl epmem_sym_before;
+    public final SymbolImpl epmem_sym_after;
+    public final SymbolImpl epmem_sym_prohibit;
 
     public EpisodicMemorySymbols(SymbolFactoryImpl syms)
     {
-        // symtab.cpp:create_predefined_symbols
-        epmem_sym = syms.createString( "smem" );
-        epmem_sym_cmd = syms.createString( "command" );
-        epmem_sym_result = syms.createString( "result" );
-        
-        // XXX needs transformation
-        
-//        thisAgent->epmem_sym = make_sym_constant( thisAgent, "epmem" );
-//        thisAgent->epmem_sym_cmd = make_sym_constant( thisAgent, "command" );
-//        thisAgent->epmem_sym_result = make_sym_constant( thisAgent, "result" );
-//
-//        thisAgent->epmem_sym_retrieved = make_sym_constant( thisAgent, "retrieved" );
-//        thisAgent->epmem_sym_status = make_sym_constant( thisAgent, "status" );
-//        thisAgent->epmem_sym_match_score = make_sym_constant( thisAgent, "match-score" );
-//        thisAgent->epmem_sym_cue_size = make_sym_constant( thisAgent, "cue-size" );
-//        thisAgent->epmem_sym_normalized_match_score = make_sym_constant( thisAgent, "normalized-match-score" );
-//        thisAgent->epmem_sym_match_cardinality = make_sym_constant( thisAgent, "match-cardinality" );
-//        thisAgent->epmem_sym_memory_id = make_sym_constant( thisAgent, "memory-id" );
+        // symtab.cpp:create_predefined_symbols       
+        epmem_sym = syms.createString("epmem" );
+        epmem_sym_cmd = syms.createString("command" );
+        epmem_sym_result = syms.createString("result" );
+
+        epmem_sym_retrieved = syms.createString("retrieved" );
+        epmem_sym_status = syms.createString("status" );
+        epmem_sym_match_score = syms.createString("match-score" );
+        epmem_sym_cue_size = syms.createString("cue-size" );
+        epmem_sym_normalized_match_score = syms.createString("normalized-match-score" );
+        epmem_sym_match_cardinality = syms.createString("match-cardinality" );
+        epmem_sym_memory_id = syms.createString("memory-id" );
         epmem_sym_present_id = syms.createString("present-id" );
-//        thisAgent->epmem_sym_no_memory = make_sym_constant( thisAgent, "no-memory" );
-//        thisAgent->epmem_sym_graph_match = make_sym_constant( thisAgent, "graph-match" );
-//        thisAgent->epmem_sym_graph_match_mapping = make_sym_constant( thisAgent, "mapping" );
-//        thisAgent->epmem_sym_graph_match_mapping_node = make_sym_constant( thisAgent, "node" );
-//        thisAgent->epmem_sym_graph_match_mapping_cue = make_sym_constant( thisAgent, "cue" );
-//        thisAgent->epmem_sym_success = make_sym_constant( thisAgent, "success" );
-//        thisAgent->epmem_sym_failure = make_sym_constant( thisAgent, "failure" );
-//        thisAgent->epmem_sym_bad_cmd = make_sym_constant( thisAgent, "bad-cmd" );
-//
-//        thisAgent->epmem_sym_retrieve = make_sym_constant( thisAgent, "retrieve" );
-//        thisAgent->epmem_sym_next = make_sym_constant( thisAgent, "next" );
-//        thisAgent->epmem_sym_prev = make_sym_constant( thisAgent, "previous" );
-//        thisAgent->epmem_sym_query = make_sym_constant( thisAgent, "query" );
-//        thisAgent->epmem_sym_negquery = make_sym_constant( thisAgent, "neg-query" );
-//        thisAgent->epmem_sym_before = make_sym_constant( thisAgent, "before" );
-//        thisAgent->epmem_sym_after = make_sym_constant( thisAgent, "after" );
-//        thisAgent->epmem_sym_prohibit = make_sym_constant( thisAgent, "prohibit" );
+        epmem_sym_no_memory = syms.createString("no-memory" );
+        epmem_sym_graph_match = syms.createString("graph-match" );
+        epmem_sym_graph_match_mapping = syms.createString("mapping" );
+        epmem_sym_graph_match_mapping_node = syms.createString("node" );
+        epmem_sym_graph_match_mapping_cue = syms.createString("cue" );
+        epmem_sym_success = syms.createString("success");
+        epmem_sym_failure = syms.createString("failure");
+        epmem_sym_bad_cmd = syms.createString("bad-cmd" );
+
+        epmem_sym_retrieve = syms.createString("retrieve" );
+        epmem_sym_next = syms.createString("next" );
+        epmem_sym_prev = syms.createString("previous" );
+        epmem_sym_query = syms.createString("query" );
+        epmem_sym_negquery = syms.createString("neg-query" );
+        epmem_sym_before = syms.createString("before" );
+        epmem_sym_after = syms.createString("after" );
+        epmem_sym_prohibit = syms.createString("prohibit" );
 
     }
 }
