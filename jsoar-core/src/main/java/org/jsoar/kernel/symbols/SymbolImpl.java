50e7b3fd767efc5a4ad5c35b06aeef15a045f924
chris.kawatsu <chris.kawatsu@soartech.com>
2013-01-31 10:17:16 -0500

    Added epmem_hash_id and epmem_valid to SymbolImpl.

--- jsoar-core/src/main/java/org/jsoar/kernel/symbols/SymbolImpl.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/symbols/SymbolImpl.java
@@ -35,6 +35,9 @@ public abstract class SymbolImpl extends EqualityTest implements Symbol
     public /*smem_hash_id*/ long smem_hash;
     public /*uintptr_t*/ long common_smem_valid;
     
+    public /*epmem_hash_id*/ long epmem_hash_id;
+    public /*uint64_t*/ long epmem_valid;
+    
     /*package*/ SymbolImpl(SymbolFactory factory, int hash_id)
     {
         this.factory = factory;
