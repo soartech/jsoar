6f8900254f15794e912bc1802993d0423ed783c9
bob.marinier <bob.marinier@soartech.com>
2013-02-21 20:32:24 -0500

    test cleanup (functionally identical)

--- jsoar-core/src/test/java/org/jsoar/kernel/wma/WmaFunctionalTests.java
+++ jsoar-core/src/test/java/org/jsoar/kernel/wma/WmaFunctionalTests.java
@@ -21,14 +21,14 @@ public class WmaFunctionalTests extends FunctionalTestHarness
     public void testSimpleActivation() throws Exception
     {
         runTest("testSimpleActivation", 2680);
-        StringWriter sw = new StringWriter();
+        final StringWriter sw = new StringWriter();
         agent.getPrinter().pushWriter(sw);
         agent.getInterpreter().eval("print s1 -i");
         final String result = sw.toString();
         
-        assert(result.indexOf("S1 ^o-from-a true [-1.5]") >= 0);
-        assert(result.indexOf("S1 ^o-from-o true [-1.9]") >= 0);
-        assert(result.indexOf("S1 ^i-from-i true [1.0]") >= 0);
-        assertEquals(result.indexOf("S1 ^o-from-i2"), -1);
+        assert(result.contains("S1 ^o-from-a true [-1.5]"));
+        assert(result.contains("S1 ^o-from-o true [-1.9]"));
+        assert(result.contains("S1 ^i-from-i true [1.0]"));
+        assert(!result.contains("S1 ^o-from-i2"));
     }
 }
