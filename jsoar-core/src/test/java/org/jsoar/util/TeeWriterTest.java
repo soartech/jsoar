3550fee78e23573675507801f44f02b68185e070
Rich Frederiksen <rdf@soartech.com>
2013-04-18 11:34:31 -0400

    Minor modification to make sure a writer gets closed.

--- jsoar-core/src/test/java/org/jsoar/util/TeeWriterTest.java
+++ jsoar-core/src/test/java/org/jsoar/util/TeeWriterTest.java
@@ -45,14 +45,22 @@ public class TeeWriterTest
         StringWriter second = new StringWriter();
         StringWriter third = new StringWriter();
         
-        TeeWriter tee = new TeeWriter(first, second, third);
-        
-        final String text = "This is some text";
-        tee.append(text);
-        tee.flush();
-        assertEquals(text, first.getBuffer().toString());
-        assertEquals(text, second.getBuffer().toString());
-        assertEquals(text, third.getBuffer().toString());
+        TeeWriter tee = null;
+        try 
+        {
+            tee = new TeeWriter(first, second, third);
+            
+            final String text = "This is some text";
+            tee.append(text);
+            tee.flush();
+            assertEquals(text, first.getBuffer().toString());
+            assertEquals(text, second.getBuffer().toString());
+            assertEquals(text, third.getBuffer().toString());
+        }
+        finally 
+        {
+            tee.close();
+        }
         
     }
 
