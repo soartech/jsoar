b25ccb42ce3642bda169712d081c5bf0c996f76c
Alex Turner <alex.turner@soartech.com>
2013-07-02 16:43:06 -0400

    Added CSoar memory statistics via the allocate command.

--- jsoar-performance-testing/src/org/jsoar/performancetesting/csoar/CSoarTest.java
+++ jsoar-performance-testing/src/org/jsoar/performancetesting/csoar/CSoarTest.java
@@ -3,6 +3,7 @@
  */
 package org.jsoar.performancetesting.csoar;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -115,7 +116,7 @@ public class CSoarTest implements Test
         // measure CSoar memory since there is no built in command to CSoar and
         // getting the runtime memory will also include the JVM therefore just
         // return 0.
-        memoryForRun = 0;
+        memoryForRun = getMemory();
 
         agent = null;
         
@@ -183,6 +184,97 @@ public class CSoarTest implements Test
     public long getMemoryForRun()
     {
         return memoryForRun;
+    }
+    
+    public long getMemory()
+    {
+        if (agent == null)
+            return 0;
+        
+        String result = agent.ExecuteCommandLine("allocate");
+        
+        /*
+         * Example Allocate Command Result
+          
+Memory pool statistics:
+
+Pool Name        Item Size  Itm/Blk  Blocks  Total Bytes
+---------------  ---------  -------  ------  -----------
+        dynamic         64      511       1        32704
+ epmem_interval         32     1023       1        32736
+   epmem_uedges         56      584       1        32704
+   epmem_pedges         64      511       1        32704
+ epmem_literals        144      227       1        32688
+   smem_id_data         40      818       1        32720
+      smem_wmes         40      818       1        32720
+  epmem_id_data         48      682       1        32736
+     epmem_wmes         40      818       1        32720
+   wma_slot_ref         40      818       0            0
+       wma_oset         40      818       0            0
+  wma_decay_set         40      818       0            0
+      wma_decay        224      146       0            0
+       rl_rules         40      818       1        32720
+          rl_et         40      818       1        32720
+     rl_id_data         40      818       1        32720
+            gds         16     2047       1        32752
+ chunk conditio         72      454       1        32688
+         io wme         40      818       1        32720
+    output link         48      682       1        32736
+     preference        192      170      27       881280
+            wme        200      163      28       912800
+           slot        216      151       1        32616
+  instantiation        104      314      10       326560
+      ms change         96      341       1        32736
+      right mem         64      511       1        32704
+          token        112      292       1        32704
+  node varnames         32     1023       1        32736
+      rete node         88      372       1        32736
+      rete test         24     1364       1        32736
+      alpha mem         80      409       1        32720
+     saved test         24     1364       1        32736
+            not         24     1364       0            0
+         action         48      682       1        32736
+     production        168      194       1        32592
+      condition         96      341      27       883872
+   complex test         16     2047       1        32752
+ float constant         80      409       1        32720
+   int constant         80      409       4       130880
+   sym constant         88      372       1        32736
+     identifier        376       87      18       588816
+       variable        112      292       1        32704
+        dynamic         24     1364       6       196416
+        dynamic         40      818       1        32720
+        dynamic         48      682       1        32736
+        dl cons         24     1364       1        32736
+      cons cell         16     2047       2        65504
+         */
+        
+        List<String> splitString = Arrays.asList(result.split("\\s+"));
+        
+        List<Long> numericsOnly = new ArrayList<Long>();
+        
+        for (int i = 0;i < splitString.size();i++)
+        {
+            try
+            {
+                Long numeric = Long.parseLong(splitString.get(i));
+                
+                numericsOnly.add(numeric);
+            }
+            catch (NumberFormatException e)
+            {} //Ignored
+        }
+        
+        long memoryUsed = 0;
+        
+        int i = 3;
+        //i is now at the number of bytes after the first 'dynamic' pool
+        for (;i < numericsOnly.size();i += 4)
+        {
+            memoryUsed += numericsOnly.get(i);
+        }
+        
+        return memoryUsed;
     }
 
     private double getKernelTime()
