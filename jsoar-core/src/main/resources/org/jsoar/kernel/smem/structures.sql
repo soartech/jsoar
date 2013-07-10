367559d652c4ad44b112015fdd2bbe674c392e8e
Alex Turner <alex.turner@soartech.com>
2013-07-10 16:43:27 -0400

    Updated all SMem SQL Statements to be the current version.

--- jsoar-core/src/main/resources/org/jsoar/kernel/smem/structures.sql
+++ jsoar-core/src/main/resources/org/jsoar/kernel/smem/structures.sql
@@ -4,37 +4,20 @@
 # are comments. If a line starts with [XXX], then that line is only executed
 # if the db driver (JDBC class name) is XXX.
 
-# TODO support other types for values.
-CREATE TABLE @PREFIX@vars (id INTEGER PRIMARY KEY,value INTEGER)
+## semantic_memory.cpp:smem_statement_container::create_tables()
 
-CREATE TABLE @PREFIX@symbols_type (id INTEGER PRIMARY KEY, sym_type INTEGER)
-
-CREATE TABLE @PREFIX@symbols_int (id INTEGER PRIMARY KEY, sym_const INTEGER)
-CREATE UNIQUE INDEX @PREFIX@symbols_int_const ON @PREFIX@symbols_int (sym_const)
-
-CREATE TABLE @PREFIX@symbols_float (id INTEGER PRIMARY KEY, sym_const REAL)
-CREATE UNIQUE INDEX @PREFIX@symbols_float_const ON @PREFIX@symbols_float (sym_const)
-
-CREATE TABLE @PREFIX@symbols_str (id INTEGER PRIMARY KEY, sym_const TEXT)
-
-CREATE UNIQUE INDEX @PREFIX@symbols_str_const ON @PREFIX@symbols_str (sym_const)
-
-CREATE TABLE @PREFIX@lti (id INTEGER PRIMARY KEY, letter INTEGER, num INTEGER, child_ct INTEGER, act_cycle INTEGER)
-
-CREATE UNIQUE INDEX @PREFIX@lti_letter_num ON @PREFIX@lti (letter, num)
-
-CREATE TABLE @PREFIX@web (parent_id INTEGER, attr INTEGER, val_const INTEGER, val_lti INTEGER, act_cycle INTEGER)
-CREATE INDEX @PREFIX@web_parent_attr_val_lti ON @PREFIX@web (parent_id, attr, val_const, val_lti)
-CREATE INDEX @PREFIX@web_attr_val_lti_cycle ON @PREFIX@web (attr, val_const, val_lti, act_cycle)
-CREATE INDEX @PREFIX@web_attr_cycle ON @PREFIX@web (attr, act_cycle)
-
-CREATE TABLE @PREFIX@ct_attr (attr INTEGER PRIMARY KEY, ct INTEGER)
-
-CREATE TABLE @PREFIX@ct_const (attr INTEGER, val_const INTEGER, ct INTEGER)
-CREATE UNIQUE INDEX @PREFIX@ct_const_attr_val ON @PREFIX@ct_const (attr, val_const)
-
-CREATE TABLE @PREFIX@ct_lti (attr INTEGER, val_lti INTEGER, ct INTEGER)
-CREATE UNIQUE INDEX @PREFIX@ct_lti_attr_val ON @PREFIX@ct_lti (attr, val_lti) 
+CREATE TABLE IF NOT EXISTS @PREFIX@versions (system TEXT PRIMARY KEY,version_number TEXT)
+CREATE TABLE @PREFIX@smem_persistent_variables (variable_id INTEGER PRIMARY KEY,variable_value INTEGER)
+CREATE TABLE @PREFIX@smem_symbols_type (s_id INTEGER PRIMARY KEY, symbol_type INTEGER)
+CREATE TABLE @PREFIX@smem_symbols_integer (s_id INTEGER PRIMARY KEY, symbol_value INTEGER)
+CREATE TABLE @PREFIX@smem_symbols_float (s_id INTEGER PRIMARY KEY, symbol_value REAL)
+CREATE TABLE @PREFIX@smem_symbols_string (s_id INTEGER PRIMARY KEY, symbol_value TEXT)
+CREATE TABLE @PREFIX@smem_lti (lti_id INTEGER PRIMARY KEY, soar_letter INTEGER, soar_number INTEGER, total_augmentations INTEGER, activation_value REAL, activations_total INTEGER, activations_last INTEGER, activations_first INTEGER)
+CREATE TABLE @PREFIX@smem_activation_history (lti_id INTEGER PRIMARY KEY, t1 INTEGER, t2 INTEGER, t3 INTEGER, t4 INTEGER, t5 INTEGER, t6 INTEGER, t7 INTEGER, t8 INTEGER, t9 INTEGER, t10 INTEGER)
+CREATE TABLE @PREFIX@smem_augmentations (lti_id INTEGER, attribute_s_id INTEGER, value_constant_s_id INTEGER, value_lti_id INTEGER, activation_value REAL)
+CREATE TABLE @PREFIX@smem_attribute_frequency (attribute_s_id INTEGER PRIMARY KEY, edge_frequency INTEGER)
+CREATE TABLE @PREFIX@smem_wmes_constant_frequency (attribute_s_id INTEGER, value_constant_s_id INTEGER, edge_frequency INTEGER)
+CREATE TABLE @PREFIX@smem_wmes_lti_frequency (attribute_s_id INTEGER, value_lti_id INTEGER, edge_frequency INTEGER)
 
 # adding an ascii table just to make lti queries easier when inspecting database
 CREATE TABLE @PREFIX@ascii (ascii_num INTEGER PRIMARY KEY, ascii_chr TEXT)
@@ -67,6 +50,15 @@ INSERT INTO @PREFIX@ascii (ascii_num, ascii_chr) VALUES (88,'X')
 INSERT INTO @PREFIX@ascii (ascii_num, ascii_chr) VALUES (89,'Y')
 INSERT INTO @PREFIX@ascii (ascii_num, ascii_chr) VALUES (90,'Z')
 
-# Finally, create the "signature" table that we use to decide whether
-# the db structure is already initialized
-CREATE TABLE @PREFIX@signature (uid INTEGER)
+## semantic_memory.cpp:smem_statement_container::create_indices()
+
+CREATE UNIQUE INDEX @PREFIX@smem_symbols_int_const ON @PREFIX@smem_symbols_integer (symbol_value)
+CREATE UNIQUE INDEX @PREFIX@smem_symbols_float_const ON @PREFIX@smem_symbols_float (symbol_value)
+CREATE UNIQUE INDEX @PREFIX@smem_symbols_str_const ON @PREFIX@smem_symbols_string (symbol_value)
+CREATE UNIQUE INDEX @PREFIX@smem_lti_letter_num ON @PREFIX@smem_lti (soar_letter, soar_number)
+CREATE INDEX @PREFIX@smem_lti_t ON @PREFIX@smem_lti (activations_last)
+CREATE INDEX @PREFIX@smem_augmentations_parent_attr_val_lti ON @PREFIX@smem_augmentations (lti_id, attribute_s_id, value_constant_s_id, value_lti_id)
+CREATE INDEX @PREFIX@smem_augmentations_attr_val_lti_cycle ON @PREFIX@smem_augmentations (attribute_s_id, value_constant_s_id, value_lti_id, activation_value)
+CREATE INDEX @PREFIX@smem_augmentations_attr_cycle ON @PREFIX@smem_augmentations (attribute_s_id, activation_value)
+CREATE UNIQUE INDEX @PREFIX@smem_wmes_constant_frequency_attr_val ON @PREFIX@smem_wmes_constant_frequency (attribute_s_id, value_constant_s_id)
+CREATE UNIQUE INDEX @PREFIX@smem_ct_lti_attr_val ON @PREFIX@smem_wmes_lti_frequency (attribute_s_id, value_lti_id)
