6bfe2fea473c7bcee50ce6b9e4bd1f5347aeabc4
ACNickels <alex.nickels@soartech.com>
2013-12-04 16:01:28 -0500

    Refactored math queries into their own package in preparation for adding more

--- jsoar-core/src/main/java/org/jsoar/kernel/smem/WeightedCueElement.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/smem/WeightedCueElement.java
@@ -8,6 +8,7 @@ package org.jsoar.kernel.smem;
 import java.util.PriorityQueue;
 
 import org.jsoar.kernel.memory.WmeImpl;
+import org.jsoar.kernel.smem.math.MathQuery;
 
 /**
  * <p>semantic_memory.h:279:smem_weighted_cue_element
