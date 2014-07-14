849aa5cef30794da40787ef78ae35b962f995b8e
ACNickels <alex.nickels@soartech.com>
2014-07-14 16:09:51 -0400

    Added a simple CLI and a test case for a preference leak problem

---
+++ jsoar-performance-testing/src/org/jsoar/performancetesting/simplecli/SimpleCLI.java
@@ -0,0 +1,63 @@
+package org.jsoar.performancetesting.simplecli;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import org.jsoar.kernel.SoarException;
+import org.jsoar.runtime.ThreadedAgent;
+
+public class SimpleCLI
+{
+
+    ThreadedAgent agent = ThreadedAgent.create("CLI Agent");
+    Scanner scanner = new Scanner(System.in);
+    
+    public static void main(String[] args) throws SoarException
+    {
+        SimpleCLI simpleCLI = new SimpleCLI();
+        simpleCLI.run();
+    }
+
+    private void run() throws SoarException
+    {
+        boolean continu = true;
+        agent.getTrace().getPrinter().addPersistentWriter(new ConsoleWriter());
+        while(continu){
+            String command = scanner.nextLine();
+            
+            switch(command){
+                case "exit":
+                    continu = false;
+                    break;
+                default:
+                    String result = "CLI failed to execute command";
+                    try{
+                        result = agent.getAgent().getInterpreter().eval(command);
+                    }catch(SoarException e){}
+                    System.out.println(result);
+                    break;
+            }
+        }
+        
+        scanner.close();
+        agent.dispose();
+    }
+    
+    private static class ConsoleWriter extends Writer{
+
+        @Override
+        public void close() throws IOException{}
+
+        @Override
+        public void flush() throws IOException{}
+
+        @Override
+        public void write(char[] cbuf, int off, int len) throws IOException
+        {
+            System.out.print(Arrays.copyOfRange(cbuf, off, off + len));
+        }
+        
+    }
+}
