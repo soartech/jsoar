ec20949799498893723678a9e1048db2fe2d6b97
Austin Brehob <austin.brehob@soartech.com>
2018-10-10 11:27:24 -0400

    Added soar stop-phase command

--- jsoar-core/src/main/java/org/jsoar/kernel/commands/SoarSettingsCommand.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/commands/SoarSettingsCommand.java
@@ -1,6 +1,7 @@
 package org.jsoar.kernel.commands;
 
 import org.jsoar.kernel.Agent;
+import org.jsoar.kernel.Phase;
 import org.jsoar.kernel.SoarException;
 import org.jsoar.kernel.SoarProperties;
 import org.jsoar.util.commands.SoarCommand;
@@ -20,7 +21,8 @@ import picocli.CommandLine.ParentCommand;
 @Command(name="soar", description="Commands and settings related to running Soar",
          subcommands={HelpCommand.class,
                       SoarSettingsCommand.Init.class,
-                      SoarSettingsCommand.MaxElaborations.class})
+                      SoarSettingsCommand.MaxElaborations.class,
+                      SoarSettingsCommand.StopPhase.class})
 public class SoarSettingsCommand implements SoarCommand, Runnable
 {
     private Agent agent;
@@ -64,7 +66,6 @@ public class SoarSettingsCommand implements SoarCommand, Runnable
         }
     }
     
-    // TODO: fix description
     @Command(name="max-elaborations", description="Maximum elaboration in a decision cycle",
             subcommands={HelpCommand.class} )
     static public class MaxElaborations implements Runnable
@@ -72,7 +73,7 @@ public class SoarSettingsCommand implements SoarCommand, Runnable
         @ParentCommand
         SoarSettingsCommand parent; // injected by picocli
         
-        @Parameters(index="0", arity = "0..1", description="The new number of maximum elaborations.")
+        @Parameters(index="0", arity = "0..1", description="The new number of maximum elaborations")
         private Integer numElaborations = null;
 
         @Override
@@ -87,7 +88,40 @@ public class SoarSettingsCommand implements SoarCommand, Runnable
             {
                 parent.agent.getProperties().set(SoarProperties.MAX_ELABORATIONS, numElaborations);
                 parent.agent.getPrinter().print("The maximum number of elaborations in a phase is now " +
-                        parent.agent.getProperties().get(SoarProperties.MAX_ELABORATIONS) + ".");
+                        numElaborations + ".");
+            }
+        }
+    }
+    
+    @Command(name="stop-phase", description="Phase before which Soar will stop",
+            subcommands={HelpCommand.class} )
+    static public class StopPhase implements Runnable
+    {
+        @ParentCommand
+        SoarSettingsCommand parent; // injected by picocli
+        
+        // These are in the same order as the corresponding entries in the Phase class
+        enum CommandPhase {input, proposal, decide, apply, output};
+        
+        @Parameters(index="0", arity = "0..1", description="Valid phases are: ${COMPLETION-CANDIDATES}")
+        private CommandPhase phase = null;
+
+        @Override
+        public void run()
+        {
+            if (phase == null)
+            {
+                CommandPhase currentPhase = CommandPhase.values()
+                        [parent.agent.getProperties().get(SoarProperties.STOP_PHASE).ordinal()];
+                parent.agent.getPrinter().print("stop-phase is " + currentPhase);
+            }
+            
+            else
+            {
+                parent.agent.getProperties().set(SoarProperties.STOP_PHASE,
+                        Phase.values()[phase.ordinal()]);
+                parent.agent.getPrinter().print("Soar will now stop before the " +
+                        phase + " phase.");
             }
         }
     }
