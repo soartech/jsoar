81c089a180250931b96614fbf7201fafb0708bab
Bob Marinier <bob.marinier@soartech.com>
2018-10-10 09:08:35 -0400

    Moved reusable parsing code to a Utils class

--- jsoar-core/src/main/java/org/jsoar/kernel/commands/SoarSettingsCommand.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/commands/SoarSettingsCommand.java
@@ -1,20 +1,11 @@
 package org.jsoar.kernel.commands;
 
-import java.io.OutputStream;
-import java.io.PrintStream;
-import java.nio.charset.Charset;
-import java.util.Arrays;
-
-import org.apache.commons.io.output.WriterOutputStream;
 import org.jsoar.kernel.Agent;
 import org.jsoar.kernel.SoarException;
 import org.jsoar.util.commands.SoarCommand;
 import org.jsoar.util.commands.SoarCommandContext;
 
-import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.RunLast;
-
 import picocli.CommandLine.HelpCommand;
 import picocli.CommandLine.ParentCommand;
 
@@ -39,15 +30,7 @@ public class SoarSettingsCommand implements SoarCommand, Runnable
     @Override
     public String execute(SoarCommandContext context, String[] args) throws SoarException
     {
-        
-        OutputStream os = new WriterOutputStream(agent.getPrinter().getWriter(), Charset.defaultCharset(), 1024, true);
-        PrintStream ps = new PrintStream(os);
-        
-        CommandLine commandLine = new CommandLine(this);
-        commandLine.parseWithHandlers(
-                new RunLast().useOut(ps),
-                CommandLine.defaultExceptionHandler().useErr(ps),
-                Arrays.copyOfRange(args, 1, args.length));
+        Utils.parseAndRun(agent, this, args);
         
         return "";
     }
