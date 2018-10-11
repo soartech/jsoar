91b45721abeaa00b846e71cf55f2ca1f7339d670
Austin Brehob <austin.brehob@soartech.com>
2018-10-11 15:41:55 -0400

    Added output log file command, fixed other output commands

--- jsoar-core/src/main/java/org/jsoar/kernel/commands/OutputCommand.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/commands/OutputCommand.java
@@ -1,5 +1,8 @@
 package org.jsoar.kernel.commands;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.LinkedList;
 
@@ -7,10 +10,12 @@ import org.jsoar.kernel.Agent;
 import org.jsoar.kernel.SoarException;
 import org.jsoar.util.commands.SoarCommand;
 import org.jsoar.util.commands.SoarCommandContext;
+import org.jsoar.util.TeeWriter;
 
 import picocli.CommandLine.Command;
 import picocli.CommandLine.HelpCommand;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 import picocli.CommandLine.ParentCommand;
 
 /**
@@ -238,6 +243,10 @@ public class SoarSettingsCommand implements SoarCommand, Runnable
     }
 }
 */
+/**
+ * This is the implementation of the "output" command.
+ * @author austin.brehob
+ */
 @Command(name="output", description="Commands related to handling output",
 subcommands={HelpCommand.class,
              OutputCommand.Log.class})
@@ -280,6 +289,9 @@ public final class OutputCommand implements SoarCommand, Runnable
         @Option(names={"-c", "--close"}, arity="0..1", description="Closes the log file")
         boolean close = false;
         
+        @Parameters(index="0", arity="0..1", description="File name")
+        String fileName = null;
+        
         @Override
         public void run()
         {
@@ -287,22 +299,56 @@ public final class OutputCommand implements SoarCommand, Runnable
             {
                 if (parent.writerStack.isEmpty())
                 {
-                    parent.agent.getPrinter().startNewLine().print("Log stack is empty");
+                    parent.agent.getPrinter().startNewLine().print("Log is not open.");
                 }
                 else
                 {
                     parent.writerStack.pop();
                     parent.agent.getPrinter().popWriter();
+                    parent.agent.getPrinter().startNewLine().print("Log file closed.");
+                }
+            }
+            else if (fileName != null)
+            {
+                if (fileName.equals("stdout"))
+                {
+                    Writer w = new OutputStreamWriter(System.out);
+                    parent.writerStack.push(null);
+                    parent.agent.getPrinter().pushWriter(new TeeWriter(
+                            parent.agent.getPrinter().getWriter(), w));
+                    parent.agent.getPrinter().startNewLine().print("Now writing to System.out");
+                }
+                else if (fileName.equals("stderr"))
+                {
+                    Writer w = new OutputStreamWriter(System.err);
+                    parent.writerStack.push(null);
+                    parent.agent.getPrinter().pushWriter(new TeeWriter(
+                            parent.agent.getPrinter().getWriter(), w));
+                    parent.agent.getPrinter().startNewLine().print("Now writing to System.err");
+                }
+                else
+                {
+                    try
+                    {
+                        Writer w = new FileWriter(fileName);
+                        parent.writerStack.push(w);
+                        parent.agent.getPrinter().pushWriter(new TeeWriter(
+                                parent.agent.getPrinter().getWriter(), w));
+                        parent.agent.getPrinter().startNewLine().print("Log file " + fileName + " open.");
+                    }
+                    catch (IOException e)
+                    {
+                        parent.agent.getPrinter().startNewLine().print(
+                                "Failed to open file '" + fileName + "': " + e.getMessage());
+                    }
                 }
             }
             else
             {
                 parent.agent.getPrinter().startNewLine().print(
-                        "log is " + (parent.writerStack.isEmpty() ? "on" : "off"));
+                        "log is " + (!parent.writerStack.isEmpty() ? "on" : "off"));
             }
         }
-        
-        
     }
     /*
     private final OptionProcessor<Options> options = OptionProcessor.create();
