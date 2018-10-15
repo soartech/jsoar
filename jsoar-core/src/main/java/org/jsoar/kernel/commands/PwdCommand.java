2bd138da1fe8c0f89f39ecfa6e91a812265b20a3
Austin Brehob <austin.brehob@soartech.com>
2018-10-15 12:03:50 -0400

    Updated pwd command to use inner class for consistency

--- jsoar-core/src/main/java/org/jsoar/kernel/commands/PwdCommand.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/commands/PwdCommand.java
@@ -12,9 +12,7 @@ import picocli.CommandLine.HelpCommand;
  * This is the implementation of the "pwd" command.
  * @author austin.brehob
  */
-@Command(name="pwd", description="Prints the working directory to the screen",
-         subcommands={HelpCommand.class})
-public class PwdCommand implements SoarCommand, Runnable
+public class PwdCommand implements SoarCommand
 {
     private final SourceCommand sourceCommand;
     private Agent agent;
@@ -28,15 +26,29 @@ public class PwdCommand implements SoarCommand, Runnable
     @Override
     public String execute(SoarCommandContext context, String[] args) throws SoarException
     {
-        Utils.parseAndRun(agent, this, args);
+        Utils.parseAndRun(agent, new Pwd(sourceCommand, agent), args);
         
         return "";
     }
 
-    @Override
-    public void run()
+    @Command(name="pwd", description="Prints the working directory to the screen",
+            subcommands={HelpCommand.class})
+    static public class Pwd implements Runnable
     {
-        agent.getPrinter().startNewLine().print(
-                sourceCommand.getWorkingDirectory().replace('\\', '/'));
+        private final SourceCommand sourceCommand;
+        private Agent agent;
+        
+        public Pwd(SourceCommand sourceCommand, Agent agent)
+        {
+            this.sourceCommand = sourceCommand;
+            this.agent = agent;
+        }
+        
+        @Override
+        public void run()
+        {
+            agent.getPrinter().startNewLine().print(
+                    sourceCommand.getWorkingDirectory().replace('\\', '/'));
+        }
     }
 }
