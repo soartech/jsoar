778847c1de16b0c5f043e6f14e916408e6ca7de2
Austin Brehob <austin.brehob@soartech.com>
2018-10-16 15:28:05 -0400

    Implemented basic parts of production firing-counts command

--- jsoar-core/src/main/java/org/jsoar/kernel/commands/ProductionCommand.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/commands/ProductionCommand.java
@@ -1,6 +1,11 @@
 package org.jsoar.kernel.commands;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.jsoar.kernel.Agent;
@@ -9,6 +14,7 @@ import org.jsoar.kernel.ProductionManager;
 import org.jsoar.kernel.ProductionType;
 import org.jsoar.kernel.SoarException;
 import org.jsoar.kernel.learning.rl.ReinforcementLearning;
+import org.jsoar.kernel.tracing.Printer;
 import org.jsoar.util.adaptables.Adaptables;
 import org.jsoar.util.commands.SoarCommand;
 import org.jsoar.util.commands.SoarCommandContext;
@@ -42,7 +48,8 @@ public class ProductionCommand implements SoarCommand
     
     @Command(name="production", description="Commands related to altering and printing production info",
             subcommands={HelpCommand.class,
-                         Excise.class})
+                         Excise.class,
+                         FiringCounts.class})
     static public class ProductionC implements Runnable
     {
         private Agent agent;
@@ -197,4 +204,137 @@ public class ProductionCommand implements SoarCommand
             }
         }
     }
+    
+    @Command(name="firing-counts", description="Print the number of times productions have fired",
+            subcommands={HelpCommand.class} )
+    static public class FiringCounts implements Runnable
+    {
+        @ParentCommand
+        ProductionC parent; // injected by picocli
+        
+        // TODO: Implement these options
+        // In CSoar, when multiple options are provided, only productions that fit into all categories
+        // are displayed along with their firing counts
+        @Option(names={"-a", "--all"}, description="Print how many times all productions fired")
+        boolean countAll = false;
+        
+        @Option(names={"-c", "--chunks"}, description="Print how many times chunks (learned rules) fired")
+        boolean countChunks = false;
+        
+        @Option(names={"-d", "--default"}, description="Print how many times default productions fired")
+        boolean countDefault = false;
+        
+        @Option(names={"-f", "--fired"}, description="Prints only rules that have fired")
+        boolean countFired = false;
+        
+        @Option(names={"-r", "--rl"}, description="Print how many times Soar-RL rules fired")
+        boolean countRL = false;
+        
+        @Option(names={"-T", "--templates"}, description="Print how many times Soar-RL templates fired")
+        boolean countTemplates = false;
+        
+        @Option(names={"-u", "--user"}, description="Print how many times user productions fired")
+        boolean countUser = false;
+        
+        @Parameters(index="0", arity="0..1", description="If an integer, list the top n productions; "
+                + "if n is 0, only the productions which haven't fired are listed. "
+                + "If not an integer, print how many times a specific production has fired.")
+        private String param = null;
+        
+        @Override
+        public void run()
+        {
+            Integer topN = null;
+            String prodName = null;
+            
+            try
+            {
+                topN = Integer.valueOf(param);
+            }
+            catch (NumberFormatException e)
+            {
+                prodName = param;
+            }
+            
+            if (topN != null)
+            {
+                if (topN == 0)
+                {
+                    List<Production> productionsNotFired = new ArrayList<Production>();
+                    for (Production p : parent.agent.getProductions().getProductions(null))
+                    {
+                        if (p.getFiringCount() == 0)
+                        {
+                            productionsNotFired.add(p);
+                        }
+                    }
+                    printResults(productionsNotFired, Integer.MAX_VALUE);
+                }
+                else
+                {
+                    printTopProductions(topN);
+                }
+            }
+            else if (prodName != null)
+            {
+                printSingleProduction(prodName);
+            }
+            else
+            {
+                if (countChunks || countDefault || countFired || countRL || countTemplates || countUser)
+                {
+                    parent.agent.getPrinter().startNewLine().print("Option(s) not "
+                            + "implemented yet. Displaying all firing counts:");
+                }
+                printTopProductions(Integer.MAX_VALUE);
+            }
+        }
+        
+        private void printResults(List<Production> productions, int n)
+        {
+            final Printer printer = parent.agent.getPrinter();
+            for (int i = 0; i < n && i < productions.size(); ++i)
+            {
+                final Production p = productions.get(i);
+                printer.startNewLine().print("%5d:  %s", p.getFiringCount(), p.getName());
+            }
+        }
+        
+        private void printSingleProduction(String name)
+        {
+            final Production p = parent.agent.getProductions().getProduction(name);
+            if (p == null)
+            {
+                parent.agent.getPrinter().startNewLine().print("No production named '" + name + "'");
+            }
+            else
+            {
+                printResults(Arrays.asList(p), 1);
+            }
+        }
+        
+        private void printTopProductions(int n)
+        {
+            final List<Production> prods = parent.agent.getProductions().getProductions(null);
+            Collections.sort(prods, new Comparator<Production>()
+            {
+                @Override
+                public int compare(Production o1, Production o2)
+                {
+                    final long d = o2.getFiringCount() - o1.getFiringCount();
+                    if (d < 0)
+                    {
+                        return -1;
+                    }
+                    else if (d > 0)
+                    {
+                        return 1;
+                    }
+                    return 0;
+                }
+            });
+            printResults(prods, n);
+        }
+    }
 }
+
