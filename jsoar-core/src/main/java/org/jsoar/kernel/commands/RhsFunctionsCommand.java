87cd9903691322c047299a4f518ec72a3d1c6a52
Austin Brehob <austin.brehob@soartech.com>
2018-10-31 17:31:17 -0400

    Updated rhs-functions command to use picocli

--- jsoar-core/src/main/java/org/jsoar/kernel/commands/RhsFunctionsCommand.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/commands/RhsFunctionsCommand.java
@@ -1,6 +1,3 @@
-/*
- * Copyright (c) 2009 Dave Ray <daveray@gmail.com>
- */
 package org.jsoar.kernel.commands;
 
 import java.util.Collections;
@@ -14,41 +11,65 @@ import org.jsoar.kernel.tracing.Printer;
 import org.jsoar.util.commands.SoarCommand;
 import org.jsoar.util.commands.SoarCommandContext;
 
+import picocli.CommandLine.Command;
+import picocli.CommandLine.HelpCommand;
+
 /**
- * Implementation of the "rhs-functions" command. Command that prints out all registered RHS functions
- * 
- * @author ray
+ * This is the implementation of the "rhs-functions" command.
+ * @author austin.brehob
  */
-public final class RhsFunctionsCommand implements SoarCommand
+public class RhsFunctionsCommand implements SoarCommand
 {
     private final Agent agent;
-
+    
     public RhsFunctionsCommand(Agent agent)
     {
         this.agent = agent;
     }
-
+    
     @Override
-    public String execute(SoarCommandContext commandContext, String[] args) throws SoarException
+    public String execute(SoarCommandContext context, String[] args) throws SoarException
     {
-        final Printer p = agent.getPrinter();
+        Utils.parseAndRun(agent, new RhsFunctions(agent), args);
         
-        p.startNewLine();
-        
-        final List<RhsFunctionHandler> handlers = agent.getRhsFunctions().getHandlers();
-        Collections.sort(handlers, new Comparator<RhsFunctionHandler>(){
-
-            @Override
-            public int compare(RhsFunctionHandler a, RhsFunctionHandler b)
-            {
-                return a.getName().compareTo(b.getName());
-            }});
-        
-        for(RhsFunctionHandler f : handlers)
-        {
-            int max = f.getMaxArguments();
-            p.print("%20s (%d, %s)%n", f.getName(), f.getMinArguments(), max == Integer.MAX_VALUE ? "*" : Integer.toString(max));
-        }
         return "";
+    }
+
+    
+    @Command(name="rhs-functions", description="Prints a list of all RHS functions",
+            subcommands={HelpCommand.class})
+    static public class RhsFunctions implements Runnable
+    {
+        private Agent agent;
+        
+        public RhsFunctions(Agent agent)
+        {
+            this.agent = agent;
+        }
+        
+        @Override
+        public void run()
+        {
+            final Printer p = agent.getPrinter();
+            p.startNewLine();
+            
+            // Obtain all RHS functions and sort them
+            final List<RhsFunctionHandler> handlers = agent.getRhsFunctions().getHandlers();
+            Collections.sort(handlers, new Comparator<RhsFunctionHandler>()
+            {
+                @Override
+                public int compare(RhsFunctionHandler a, RhsFunctionHandler b)
+                {
+                    return a.getName().compareTo(b.getName());
+                }
+            });
+            
+            for (RhsFunctionHandler f : handlers)
+            {
+                int max = f.getMaxArguments();
+                p.print("%20s (%d, %s)%n", f.getName(), f.getMinArguments(),
+                        max == Integer.MAX_VALUE ? "*" : Integer.toString(max));
+            }
+        }
     }
 }
