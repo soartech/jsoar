868b47ab16262d8f4074c641ad3a5a22c89277eb
Tim Saucer <timothy.saucer@soartech.com>
2019-04-22 11:43:41 -0400

    Change scope of two methods in DefaultProductionManager.

--- jsoar-core/src/main/java/org/jsoar/kernel/DefaultProductionManager.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/DefaultProductionManager.java
@@ -75,7 +75,7 @@ public class DefaultProductionManager implements ProductionManager
         this.context = context;
     }
     
-    void initialize()
+    public void initialize()
     {
         this.syms = Adaptables.require(getClass(), context, SymbolFactoryImpl.class);
         this.rete = Adaptables.require(getClass(), context, Rete.class);
@@ -85,7 +85,7 @@ public class DefaultProductionManager implements ProductionManager
     /**
      * <p>init_soar.cpp:297:reset_statistics
      */
-    void resetStatistics()
+    public void resetStatistics()
     {
         // reset_production_firing_counts(thisAgent);
         for (Production p : this.productionsByName.values())
