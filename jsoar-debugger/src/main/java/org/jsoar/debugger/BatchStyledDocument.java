345d567954893e1397d4ea24aef74b95c6480bfb
Rich Frederiksen <rdf@soartech.com>
2019-06-20 00:57:40 -0400

    Removed unused code, added blocking queue to keep highlight thread from spinnings and other minor modifications.
    Also removed some unnecessary default style inserts.

--- jsoar-debugger/src/main/java/org/jsoar/debugger/BatchStyledDocument.java
+++ jsoar-debugger/src/main/java/org/jsoar/debugger/BatchStyledDocument.java
@@ -2,7 +2,8 @@ package org.jsoar.debugger;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.TreeSet;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.swing.JTextPane;
@@ -21,188 +22,165 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BatchStyledDocument extends DefaultStyledDocument {
-	private static final long serialVersionUID = 1L;
-	private static final Logger logger = LoggerFactory.getLogger(BatchStyledDocument.class);
-	
-	private final Highlighter highlighter;
-	private final JSoarDebugger debugger;
-	private final AtomicBoolean colorImmediately;
-	private final Object colorLock;
-	
-	public BatchStyledDocument(Highlighter highlighter, JSoarDebugger debugger, AtomicBoolean colorImmediately, Object colorLock) {
-		this.highlighter = highlighter;
-		this.debugger = debugger;
-		highlightingThread.setDaemon(true);
-		highlightingThread.start();
-		this.colorImmediately = colorImmediately;
-		this.colorLock = colorLock;
-	}
-	public void takeBatchUpdate(ElementSpec[] elements, JTextPane outputWindow) {
-		takeBatchUpdate(elements, outputWindow, false);
-	}
+    private static final long serialVersionUID = 1L;
+    private static final Logger logger = LoggerFactory.getLogger(BatchStyledDocument.class);
 
-	private static final DefaultStyledDocument emptyDocument = new DefaultStyledDocument();
+    private final Highlighter highlighter;
+    private final JSoarDebugger debugger;
+    private final AtomicBoolean colorImmediately;
+    private final Object colorLock;
 
-	// If you pass in the output window, it will be scrolled to the end
-	public void takeBatchUpdate(ElementSpec[] elements, JTextPane outputWindow, boolean scrollToBottom) {
-		final DefaultStyledDocument self = this;
-		try {
-			SwingUtilities.invokeAndWait(new Runnable() {
-				@Override
-				public void run() {
-					outputWindow.setDocument(emptyDocument);
-					try {
-						// This function is protected
-						insert(getEndPosition().getOffset(), elements);
-					} catch (BadLocationException e) {
-						e.printStackTrace();
-					}
-					outputWindow.setDocument(self);
-					if (scrollToBottom) {
-						// Scroll to the end
-						outputWindow.setCaretPosition(outputWindow.getDocument().getLength());
-					}
-				}
-			});
-		} catch (InvocationTargetException | InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    public BatchStyledDocument(Highlighter highlighter, JSoarDebugger debugger, AtomicBoolean colorImmediately, Object colorLock) {
+        this.highlighter = highlighter;
+        this.debugger = debugger;
+        highlightingThread.setDaemon(true);
+        highlightingThread.start();
+        this.colorImmediately = colorImmediately;
+        this.colorLock = colorLock;
+    }
 
-	private final class NewText {
-		public final int offset;
-		public final String text;
+    private final class NewText {
+        public final int offset;
+        public final String text;
 
-		public NewText(int offset, String text) {
-			this.offset = offset;
-			this.text = text;
-		}
-	}
+        public NewText(int offset, String text) {
+            this.offset = offset;
+            this.text = text;
+        }
+    }
 
-	private final ConcurrentLinkedQueue<NewText> newTexts = new ConcurrentLinkedQueue<>();
+    private final BlockingQueue<NewText> newTexts = new LinkedBlockingQueue<>();
 
-	public void takeDelayedBatchUpdate(String text, JTextPane outputWindow, boolean scrollToBottom) {
-		Runnable runnable = new Runnable() {
-			@Override
-			public void run() {
-//			outputWindow.setDocument(emptyDocument);
-				int offset = getEndPosition().getOffset();
-				try {
-					//This function is protected
-					insertString(offset, text, highlighter.getDefaultAttributes());
-					//This needs to be on the EDT where we still know the offset is correct -ACN
-					newTexts.offer(new NewText(offset, text));
-				} catch (BadLocationException e) {
-					e.printStackTrace();
-				}
-//			outputWindow.setDocument(self);
-				if (scrollToBottom) {
-		            // Scroll to the end
-		            outputWindow.setCaretPosition(outputWindow.getDocument().getLength());
-		        }
-			}
-		};
-			//On close, this gets purged from the EDT which will cause a deadlock. -ACN
-		if (SwingUtilities.isEventDispatchThread()) {
-			runnable.run();
-		} else {
-			try {
-				SwingUtilities.invokeAndWait(runnable);
-			} catch (InvocationTargetException e) {
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				//Probably means we're shutting down
-				logger.info("The following text was not printed due to thread interrupt.  If you see this message at shutdown, it is safely ignored. " + text);
-			}
-		}
-	}
+    public void takeDelayedBatchUpdate(String text, JTextPane outputWindow, boolean scrollToBottom) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+//                int offset = getEndPosition().getOffset();
+//                offset--;
+//                //BatchStyledDocument.logger.info("Offset = {} {} {}", offset, getEndPosition(), getLength());
+//                try {
+//                    //This function is protected
+//                    insertString(offset, text, highlighter.getDefaultAttributes());
+//                    //This needs to be on the EDT where we still know the offset is correct -ACN
+//                    boolean result = newTexts.offer(new NewText(offset, text));
+//                    if ( !result ) {
+//                        logger.error("This should never block so you should never see this message!");
+//                    }
+//                } catch (BadLocationException e) {
+//                    BatchStyledDocument.logger.info("Offset = {} {} {}", offset, getEndPosition(), getLength());
+//                    e.printStackTrace();
+//                }
+                if (scrollToBottom) {
+                    // Scroll to the end
+                    BatchStyledDocument.this.writeLock();
+                    outputWindow.setCaretPosition(outputWindow.getDocument().getLength());
+                    BatchStyledDocument.this.writeUnlock();
+                }
+            }
+        };
+        
+        writeLock();
+        int offset = getEndPosition().getOffset();
+        offset--;
+        //BatchStyledDocument.logger.info("Offset = {} {} {}", offset, getEndPosition(), getLength());
+        try {
+            // This function is protected and gets it's own writelocks but they are cheap if you already have them
+            insertString(offset, text, highlighter.getDefaultAttributes());
+            // This needs to be between writelocks to keep the offset correct
+            boolean result = newTexts.offer(new NewText(offset, text));
+            if ( !result ) {
+                logger.error("This should never block so you should never see this message!");
+            }
+        } catch (BadLocationException e) {
+            BatchStyledDocument.logger.info("Offset = {} {} {}", offset, getEndPosition(), getLength());
+            e.printStackTrace();
+        } finally {
+            writeUnlock();
+        }
+        
+        //On close, this gets purged from the EDT which will cause a deadlock. -ACN
+        if (SwingUtilities.isEventDispatchThread()) {
+            runnable.run();
+        } else {
+            try {
+                SwingUtilities.invokeAndWait(runnable);
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                //Probably means we're shutting down
+                logger.info("The following text was not printed due to thread interrupt.  If you see this message at shutdown, it is safely ignored. " + text);
+            }
+        }
+    }
 
-	private final BatchStyledDocument self = this;
-	private final Thread highlightingThread = new Thread("Document Highlight Thread"){
-		public void run() {
-			while(!Thread.interrupted()) {
-				//If this isn't the active thread, don't act or block 
-				if(!colorImmediately.get()) {try {
-					Thread.sleep(0);
-					continue;
-				} catch (InterruptedException e) {}}
-				synchronized (colorLock) {
-					NewText newText = newTexts.poll();
-					if (newText == null) {
-						Thread.yield();
-						continue;
-					}
-					final TreeSet<StyleOffset> styles = highlighter.getPatterns().getForAll(newText.text, debugger);
-					if (styles.isEmpty()) {
-						// no matches, just print the text
-						// elements.add(new ElementSpec(highlighter.getDefaultAttributes(),
-						// ElementSpec.ContentType, string.toCharArray(), 0, string.length()));
-					} else {
-						int index = 0;
-						// FIXME - this is SLOW!!!!
-						for (StyleOffset offset : styles) {
-							int start = offset.start;
-							int end = offset.end;
-							if (start >= end || start < index) {
-								System.out.println("hit");
-								continue;
-							}
+    private final BatchStyledDocument self = this;
+    private final Thread highlightingThread = new Thread("Document Highlight Thread"){
+        public void run() {
+            while(!Thread.interrupted()) {
+                synchronized (colorLock) {
+                    NewText newText = null;
+                    try {
+                        newText = newTexts.take();
+                    } catch (InterruptedException e) {
+                        logger.error("Syntax highlighting thread interrupted!", e);
+                        continue;
+                    }
+                    
+                    // This shouldn't happen but just in case
+                    if (newText == null) {
+                        logger.error("Retrieved null from text queue!");
+                        continue;
+                    }
+                    
+                    //logger.info("DHT received new text |{}|", newText.text);
+                    final TreeSet<StyleOffset> styles = highlighter.getPatterns().getForAll(newText.text, debugger);
+                    if ( !styles.isEmpty( )) {
+                        int index = 0;
+                        // FIXME - this is SLOW!!!!
+                        for (StyleOffset offset : styles) {
+                            int start = offset.start;
+                            int end = offset.end;
+                            if (start >= end || start < index) {
+                                // At the moment I'm not sure if this is an error or something else...
+//                                BatchStyledDocument.logger.info("hit strange style offset values start={}, end={}, index={}"
+//                                        ,start
+//                                        ,end,
+//                                        index);
+                                continue;
+                            }
 
-							writeLock();
-							// the stuff between the match
-							self.setCharacterAttributesUnsafe(newText.offset + index, start - index,
-									highlighter.getDefaultAttributes(), true);
+                            //writeLock();
+                            // the stuff between the match
+                            //self.setCharacterAttributesUnsafe(newText.offset + index, start - index,
+                            //        highlighter.getDefaultAttributes(), true);
 
-							// elements.add(
-							// new ElementSpec(
-							// highlighter.getDefaultAttributes(),
-							// ElementSpec.ContentType,
-							// string.substring(index, start).toCharArray(),
-							// 0,
-							// start - index));
-							// the matched stuff
-							self.setCharacterAttributesUnsafe(newText.offset + index + start - index, end - start,
-									offset.style, true);
-							// elements.add(
-							// new ElementSpec(
-							// offset.style,
-							// ElementSpec.ContentType,
-							// string.substring(start, end).toCharArray(),
-							// 0,
-							// end - start));
-							index = end;
-							writeUnlock();
-						}
-						if (index < newText.text.length()) {
-							// trailing text after all matches
-							self.setCharacterAttributes(newText.offset + index, newText.text.length() - index,
-									highlighter.getDefaultAttributes(), true);
-							// elements.add(
-							// new ElementSpec(
-							// highlighter.getDefaultAttributes(),
-							// ElementSpec.ContentType,
-							// string.substring(index).toCharArray(),
-							// 0,
-							// string.length() - index));
-						}
-					}
-				}
-//		        elements.add(new ElementSpec(null, ElementSpec.EndTagType));
-//		        elements.add(new ElementSpec(highlighter.getDefaultAttributes(), ElementSpec.StartTagType));
-			}
-			throw new RuntimeException("Highlighting thread has died");
-		};
-	};
-	
-	
-	private final void setCharacterAttributesUnsafe(int offset, int length, AttributeSet s, boolean replace) {
+                            // the matched stuff
+                            self.setCharacterAttributes(newText.offset + index + start - index, end - start,
+                                    offset.style, true);
+
+                            index = end;
+                            //writeUnlock();
+                        }
+//                        if (index < newText.text.length()) {
+//                            // trailing text after all matches
+//                            self.setCharacterAttributes(newText.offset + index, newText.text.length() - index,
+//                                    highlighter.getDefaultAttributes(), true);
+//                        }
+                    }
+                }
+            }
+            throw new RuntimeException("Highlighting thread has died");
+        };
+    };
+
+    private final void setCharacterAttributesUnsafe(int offset, int length, AttributeSet s, boolean replace) {
         if (length == 0) {
             return;
         }
         try {
-//            writeLock();
+            //            writeLock();
             DefaultDocumentEvent changes =
-                new DefaultDocumentEvent(offset, length, DocumentEvent.EventType.CHANGE);
+                    new DefaultDocumentEvent(offset, length, DocumentEvent.EventType.CHANGE);
 
             // split elements that need it
             buffer.change(offset, length, changes);
@@ -229,8 +207,7 @@ public class BatchStyledDocument extends DefaultStyledDocument {
             fireChangedUpdate(changes);
             fireUndoableEditUpdate(new UndoableEditEvent(this, changes));
         } finally {
-//            writeUnlock();
+            //            writeUnlock();
         }
-
     }
 }
