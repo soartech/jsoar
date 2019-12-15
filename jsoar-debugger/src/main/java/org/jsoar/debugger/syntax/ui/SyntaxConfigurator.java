0ddae1d08c2ae5cffeda7e0187c167bc95722bb0
Rich Frederiksen <rdf@soartech.com>
2019-12-15 00:27:23 -0500

    Whitespace change.

--- jsoar-debugger/src/main/java/org/jsoar/debugger/syntax/ui/SyntaxConfigurator.java
+++ jsoar-debugger/src/main/java/org/jsoar/debugger/syntax/ui/SyntaxConfigurator.java
@@ -233,7 +233,6 @@ public class SyntaxConfigurator {
             }
         });
 
-
         btnApply.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
