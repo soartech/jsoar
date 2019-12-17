763b50a5af171545ad9a10aad9e564afa6212316
Bob Marinier <bob.marinier@soartech.com>
2019-12-17 10:10:31 -0500

    Removed unused import

--- jsoar-tcl/src/main/java/org/jsoar/tcl/SoarTclInterface.java
+++ jsoar-tcl/src/main/java/org/jsoar/tcl/SoarTclInterface.java
@@ -17,7 +17,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
