37f838e10f39b6e0b4fd4ad16895fa3dc107914e
Bob Marinier <bob.marinier@soartech.com>
2020-04-20 14:06:22 -0400

    Updated PicocliSoarCommand so the agent is optional. Updated pwd back to not needing an agent.
    There are some commands for which an agent is unnatural. E.g., pwd really wants a SoarCommand, which ultimately is associated with an agent, but the agent is not accessible. If we had to separately supply an agent, we could end up in a situation where two different agents were accidentally used, which could only be bad. By making PicocliSoarCommand more flexible, we avoid this issue.

--- jsoar-core/src/main/java/org/jsoar/util/commands/PicocliSoarCommand.java
+++ jsoar-core/src/main/java/org/jsoar/util/commands/PicocliSoarCommand.java
@@ -11,18 +11,35 @@ public abstract class PicocliSoarCommand implements SoarCommand
     protected final static String NULL = null;
     
     final protected Agent agent;
-    protected Object picocliCommand;
+    final protected Object picocliCommand;
     final protected CommandLine commandLine;
     
+    /**
+     * Creates a PicocliSoarCommand with an agent.
+     * A command with an agent may print results to the agent's printer
+     * and/or return a result directly.
+     */
     public PicocliSoarCommand(Agent agent, Object picocliCommand) {
         this.agent = agent;
         this.picocliCommand = picocliCommand;
         this.commandLine = new CommandLine(picocliCommand);
     }
     
+    /**
+     * Creates a PicocliSoarCommand with no agent.
+     * A command with no agent is expected to return any result directly.
+     */
+    public PicocliSoarCommand(Object picocliCommand) {
+        this(null, picocliCommand);
+    }
+    
     @Override
     public String execute(SoarCommandContext context, String[] args) throws SoarException {
-        return Utils.parseAndRun(this.agent, this.commandLine, args);
+        if(agent != null) {
+            return Utils.parseAndRun(this.agent, this.commandLine, args);
+        } else {
+            return Utils.parseAndRun(this.commandLine, args);
+        }
     }
 
     @Override
