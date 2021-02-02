6849abe930eb3e8e366c127f0a9f8d7b43b2b898
Bob Marinier <bob.marinier@soartech.com>
2021-02-02 16:18:17 -0500

    Increased time for test to let multiple agents start
    This seems to fail a lot on github's windows latest java 8 runner

--- jsoar-core/src/test/java/org/jsoar/runtime/ThreadedAgentTest.java
+++ jsoar-core/src/test/java/org/jsoar/runtime/ThreadedAgentTest.java
@@ -245,7 +245,7 @@ public class ThreadedAgentTest
 
         // Give the agents a chance to start
         logger.debug("Giving agents a chance to start");
-        Thread.sleep(500);
+        Thread.sleep(2000);
 
         // Make sure the agents are running
         for (ThreadedAgent ta : agents)
