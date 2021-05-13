09220bcf5386a99388a34171d9b5dad92191c5b9
markschenk <mark.schenk@independentip.com>
2021-05-13 08:11:52 +0200

    Removed checkNotNull from Arguments

--- jsoar-core/src/main/java/org/jsoar/util/Arguments.java
+++ jsoar-core/src/main/java/org/jsoar/util/Arguments.java
@@ -7,19 +7,8 @@ package org.jsoar.util;
 
 /** @author ray */
 public final class Arguments {
-  /**
-   * Throw an illegal argument exception if the given argument is {@code null}
-   *
-   * @param arg the argument to check
-   * @param name the name of the argument to be used in the exception message.
-   * @deprecated replaced by @NonNull of lombok
-   */
-  public static void checkNotNull(Object arg, String name) {
-    if (arg == null) {
-      throw new IllegalArgumentException("'" + name + "' must not be null");
-    }
-  }
 
+  private Arguments() {}
   /**
    * If the given condition is not true, throw an {@link IllegalArgumentException} with the given
    * description.
