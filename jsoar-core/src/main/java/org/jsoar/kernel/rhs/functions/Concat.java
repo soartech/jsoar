2414bd2184743cac60f3ea698ddde3297401b5b0
markschenk <mark.schenk@independentip.com>
2021-05-17 08:09:11 +0200

    Extended testing functionality of Concat production. Also include non-happy flows and added validation of input arguments

--- jsoar-core/src/main/java/org/jsoar/kernel/rhs/functions/Concat.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/rhs/functions/Concat.java
@@ -6,6 +6,9 @@
 package org.jsoar.kernel.rhs.functions;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import lombok.NonNull;
 import org.jsoar.kernel.symbols.Symbol;
 
 /**
@@ -23,12 +26,8 @@ public class Concat extends AbstractRhsFunctionHandler {
    * @param arguments List of symbols
    * @return Concatenated symbols as a string
    */
-  public static String concat(List<Symbol> arguments) {
-    StringBuilder result = new StringBuilder();
-    for (Symbol s : arguments) {
-      result.append(String.format("%#s", s));
-    }
-    return result.toString();
+  public static String concat(@NonNull List<Symbol> arguments) {
+    return arguments.stream().map(Objects::toString).collect(Collectors.joining());
   }
 
   public Concat() {
@@ -39,7 +38,7 @@ public class Concat extends AbstractRhsFunctionHandler {
    * @see org.jsoar.kernel.rhs.functions.RhsFunctionHandler#execute(org.jsoar.kernel.symbols.SymbolFactory, java.util.List)
    */
   @Override
-  public Symbol execute(RhsFunctionContext context, List<Symbol> arguments)
+  public Symbol execute(@NonNull RhsFunctionContext context, List<Symbol> arguments)
       throws RhsFunctionException {
     return context.getSymbols().createString(concat(arguments));
   }
--- jsoar-core/src/test/java/org/jsoar/kernel/rhs/functions/ConcatTest.java
+++ jsoar-core/src/test/java/org/jsoar/kernel/rhs/functions/ConcatTest.java
@@ -6,26 +6,61 @@
 package org.jsoar.kernel.rhs.functions;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
 import java.util.List;
 import org.jsoar.JSoarTest;
+import org.jsoar.kernel.symbols.Symbol;
 import org.jsoar.kernel.symbols.Symbols;
 import org.junit.Test;
 
 /** @author ray */
 public class ConcatTest extends JSoarTest {
 
-  /**
-   * Test method for {@link org.jsoar.kernel.rhs.functions.Concat#execute(RhsFunctionContext,
-   * List)}.
-   */
+  @Test
+  public void testConstructor() {
+    Concat concat = new Concat();
+    assertEquals("concat", concat.getName());
+    assertEquals(0, concat.getMinArguments());
+    assertEquals(Integer.MAX_VALUE, concat.getMaxArguments());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConcatThrowsExceptionIfArgumentsIsNull() throws RhsFunctionException {
+    Concat.concat(null);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testExecuteThrowsExceptionIfContextIsNull() throws RhsFunctionException {
+    Concat c = new Concat();
+    c.execute(null, List.of(mock(Symbol.class)));
+  }
+
   @Test
   public void testExecute() throws Exception {
-    Concat c = new Concat();
-    assertEquals(
-        "abc  D  e><>123",
-        c.execute(rhsFuncContext, Symbols.asList(syms, "a", "b", "c  D  e>", "<>", 1, 2, 3))
-            .asString()
-            .getValue());
+    // Given a instance of production concat
+    Concat concat = new Concat();
+    // And a list of symbols
+    List<Symbol> arguments = Symbols.asList(syms, "a", "b", "c  D  e>", "<>", 1, 2, 3);
+
+    // When execute concat production
+    Symbol result = concat.execute(rhsFuncContext, arguments);
+
+    // Then result matches concatenated list of symbols
+    assertEquals("abc  D  e><>123", result.asString().getValue());
+  }
+
+  @Test
+  public void testExecuteIfArgumentsEmpty() throws Exception {
+    // Given a instance of production concat
+    Concat concat = new Concat();
+    // And a empty list of symbols
+    List<Symbol> arguments = Symbols.asList(syms);
+
+    // When execute concat production
+    Symbol result = concat.execute(rhsFuncContext, arguments);
+
+    // Then result matches concatenated list of symbols
+    assertEquals("", result.asString().getValue());
   }
 }
