70cfc5620ceb0ae95ef49674ec7d130bcc3c382e
markschenk <mark.schenk@independentip.com>
2021-05-27 06:48:53 +0200

    Improved implementation of BooleanTypeConverter and extended test coverage

--- jsoar-core/src/main/java/org/jsoar/kernel/commands/BooleanTypeConverter.java
+++ jsoar-core/src/main/java/org/jsoar/kernel/commands/BooleanTypeConverter.java
@@ -1,26 +1,19 @@
 package org.jsoar.kernel.commands;
 
-import picocli.CommandLine;
+import lombok.NonNull;
 import picocli.CommandLine.ITypeConverter;
-import picocli.CommandLine.ParameterException;
-import picocli.CommandLine.PicocliException;
 
 public class BooleanTypeConverter implements ITypeConverter<Boolean> {
 
   @Override
-  public Boolean convert(String value) throws Exception {
-    if (value.equalsIgnoreCase("true")
-        || value.equalsIgnoreCase("yes")
-        || value.equalsIgnoreCase("enable")
-        || value.equalsIgnoreCase("on")) {
-      return Boolean.TRUE;
-    } else if (
-        value.equalsIgnoreCase("false")
-        || value.equalsIgnoreCase("no")
-        || value.equalsIgnoreCase("disable")
-        || value.equalsIgnoreCase("off")) {
-      return Boolean.FALSE;
-    } else
-      throw new PicocliException("Expected one argument: on | off");
+  public Boolean convert(@NonNull String value) {
+    switch (value.toLowerCase()) {
+      case "true", "yes", "enable", "on":
+        return Boolean.TRUE;
+      case "false", "no", "disable", "off":
+        return Boolean.FALSE;
+      default:
+        throw new IllegalArgumentException("Expected one argument: on | off");
     }
+  }
 }
---
+++ jsoar-core/src/test/java/org/jsoar/kernel/commands/BooleanTypeConverterTest.java
@@ -0,0 +1,76 @@
+package org.jsoar.kernel.commands;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BooleanTypeConverterTest {
+
+  @Test
+  public void testConvertValueEnabled() {
+    convertValue("enable", true);
+    convertValue("ENABLE", true);
+  }
+
+  @Test
+  public void testConvertValueDisable() {
+    convertValue("disable", false);
+    convertValue("DISABLE", false);
+  }
+
+  @Test
+  public void testConvertValueOn() {
+    convertValue("on", true);
+    convertValue("ON", true);
+  }
+
+  @Test
+  public void testConvertValueOff() {
+    convertValue("off", false);
+    convertValue("OFF", false);
+  }
+
+  @Test
+  public void testConvertValueYes() {
+    convertValue("yes", true);
+    convertValue("YES", true);
+  }
+
+  @Test
+  public void testConvertValueNo() {
+    convertValue("no", false);
+    convertValue("NO", false);
+  }
+
+  @Test
+  public void testConvertValueTrue() {
+    convertValue("true", true);
+    convertValue("TRUE", true);
+  }
+
+  @Test
+  public void testConvertValueFalse() {
+    convertValue("false", false);
+    convertValue("FALSE", false);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConvertThrowsExceptionIfValueIsNull() {
+    BooleanTypeConverter converter = new BooleanTypeConverter();
+    converter.convert(null);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testConvertThrowsExceptionIfValueInvalid() {
+    BooleanTypeConverter converter = new BooleanTypeConverter();
+    converter.convert("invalid");
+  }
+
+  private void convertValue(String value, boolean expectedResult) {
+    BooleanTypeConverter converter = new BooleanTypeConverter();
+    boolean result = converter.convert(value);
+    assertEquals(expectedResult, result);
+  }
+
+
+}
