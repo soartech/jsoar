soartech/jsoar

commit cc9f6bee77baf13b8e0d24f753c97f91ec1a9f6d

Converted tons of anonymous classes to lambdas to reduce boilerplate improve readability

jsoar-debugger/src/main/java/org/jsoar/debugger/WmeSupportView.java
@@ -148,25 +148,18 @@ public class WmeSupportView extends AbstractAdaptableView implements SelectionLi
         {
             return;
         }
-        final Callable<WmeSupportInfo> call = new Callable<WmeSupportInfo>() {
-
-            @Override
-            public WmeSupportInfo call() throws Exception
-            {
-                final Agent agent = debugger.getAgent().getAgent();
-                return WmeSupportInfo.get(agent, w);
-            }};
-        final CompletionHandler<WmeSupportInfo> finish = new CompletionHandler<WmeSupportInfo>() {
-
-            @Override
-            public void finish(WmeSupportInfo sourceInfo)
-            {
-                source.setText(String.format("<html><b>&nbsp;<code> %#s</code></b> is supported by:</html>", w));
-                entryList.setModel(SwingTools.addAll(new DefaultListModel<Support>(), sourceInfo.getSupports()));
-                wmeModel.setWmes(null);
-                wmeTable.packAll();
-                entryList.setSelectedIndex(0);
-            }
+        
+        final Callable<WmeSupportInfo> call = () -> {
+            final Agent agent = debugger.getAgent().getAgent();
+            return WmeSupportInfo.get(agent, w);
+        };
+        
+        final CompletionHandler<WmeSupportInfo> finish = result -> {
+            source.setText(String.format("<html><b>&nbsp;<code> %#s</code></b> is supported by:</html>", w));
+            entryList.setModel(SwingTools.addAll(new DefaultListModel<Support>(), sourceInfo.getSupports()));
+            wmeModel.setWmes(null);
+            wmeTable.packAll();
+            entryList.setSelectedIndex(0);
         };
         debugger.getAgent().execute(call, SwingCompletionHandler.newInstance(finish));
     }

commit 55f36d641cea9ea754a2209c60bded1bf5416566

Fixed NPE in wme support view
The bug was introduced in commit cc9f6bee77baf13b8e0d24f753c97f91ec1a9f6d

jsoar-debugger/src/main/java/org/jsoar/debugger/WmeSupportView.java
@@ -154,7 +154,7 @@ public class WmeSupportView extends AbstractAdaptableView implements SelectionLi
             return WmeSupportInfo.get(agent, w);
         };
         
-        final CompletionHandler<WmeSupportInfo> finish = result -> {
+        final CompletionHandler<WmeSupportInfo> finish = sourceInfo -> {
             source.setText(String.format("<html><b>&nbsp;<code> %#s</code></b> is supported by:</html>", w));
             entryList.setModel(SwingTools.addAll(new DefaultListModel<Support>(), sourceInfo.getSupports()));
             wmeModel.setWmes(null);