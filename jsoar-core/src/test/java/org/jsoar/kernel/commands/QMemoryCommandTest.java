08b06447067ea9ad78407f23ce451feec5468443
Bob Marinier <bob.marinier@soartech.com>
2022-10-30 12:32:22 -0400

    Narrow the scope of the test to more precisely show where things go wrong.
    Only one autocomplete call is needed.

--- jsoar-core/src/test/java/org/jsoar/kernel/commands/QMemoryCommandTest.java
+++ jsoar-core/src/test/java/org/jsoar/kernel/commands/QMemoryCommandTest.java
@@ -9,7 +9,6 @@ import java.util.concurrent.TimeoutException;
 
 import org.jsoar.kernel.Agent;
 import org.jsoar.kernel.SoarException;
-import org.jsoar.runtime.ThreadedAgent;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -50,23 +49,19 @@ public class QMemoryCommandTest
     {
 
         // this command should echo it's last argument to output
-        executeCommand("qmemory --set a foo");
+        this.agent.getInterpreter().eval("qmemory --set a foo");
         
         // check output is what's expected
         assertEquals("foo", this.outputWriter.toString().trim());
         clearBuffer();
         
         // this command should not echo anything to output
-        executeCommand("qmemory --clear");
+        this.autoComplete("qmemory --clear");
+        this.agent.getInterpreter().eval("qmemory --clear");
         
         // confirm there is no output
         // as of this writing, the output is not empty -- it appears to remember its args from the previous execution of the command and does that one again
         assertEquals(0, this.outputWriter.toString().length());
-    }
-    
-    protected void executeCommand(String command) throws SoarException {
-        this.autoComplete(command);
-        this.agent.getInterpreter().eval(command);
     }
     
     /**
